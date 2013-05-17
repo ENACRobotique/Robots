94e4ba34089d897fbd6f5a0c2c662ad965ad4595
Ludovic Lacoste <ludolacost@gmail.com>
2013-05-17 16:24:23 +0200

    Moved middle level functions to standalone library (../lpc_lib).

--- eint.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EINT_H
-#define _EINT_H
-
-void eint_init(void (*isr_14)(), void (*isr_17)());
-
-#endif
--- uart0.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UART0_H
-#define _UART0_H
-
-void uart0_init(unsigned int baud, unsigned char *rxc);
-
-#endif
