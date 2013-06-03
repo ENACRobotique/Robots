68c4c06e6734a0393adc32a712161b7674623daf
Ludovic Lacoste <ludolacost@gmail.com>
2013-06-04 01:45:17 +0200

    [communication/superBus_sniffer] Renames the project to reflect its reality. Suppresses compilation warnings.

--- communication/superBus_linux_XBeeSniffer/src/superBuf_sniffer.c
+++ communication/superBus_linux_XBeeSniffer/src/superBuf_sniffer.c
@@ -9,33 +9,43 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
+#include <sys/time.h>
 
 #include "messages.h"
 #include "network_cfg.h"
 #include "lib_Xbee_x86.h"
 
-int bool=42;
 unsigned char srcMask=0xFF, dstMask=0xFF;
 char dType[E_TYPE_COUNT], rawDisplay=0;
 int snetOnly=1; //display only message which destination is within our subnetwork
+sig_t prevH = NULL;
 
-void exitHandler(int rien){
+void sigHandler(int sig){
     char car;
     int i;
     int menuloop=1;
     do{
-        printf("\nwaiting your orders:\n m: edit destination address mask (current %x)\n M: edit source address mask (current %x)\n t: add one visible type\n r: remove one visible type\n R: remove all visible types\n d: display current parameters\n s: display only message which destination is in the debugger subnetwork\n c: close this menu\n q: quit the program\n",dstMask, srcMask);
+        printf("\nwaiting your orders:\n"
+        		" m: edit destination address mask (current %x)\n"
+        		" M: edit source address mask (current %x)\n"
+        		" t: add one visible type\n"
+        		" r: remove one visible type\n"
+        		" R: remove all visible types\n"
+        		" d: display current parameters\n"
+        		" s: display only message which destination is in the debugger subnetwork\n"
+        		" c: close this menu\n"
+        		" q: quit the program\n",dstMask, srcMask);
         car=getchar();
         while(getchar()!='\n');
-        switch (car ){
+        switch(car){
         case 'm':
             printf("current destination mask: %x\n new mask ?\n",srcMask);
-            scanf("%2x",&dstMask);
+            scanf("%2hhx",&dstMask);
             while(getchar()!='\n');
             break;
         case 'M':
             printf("current source mask: %x\n new mask ?\n",srcMask);
-            scanf("%2x",&srcMask);
+            scanf("%2hhx",&srcMask);
             while(getchar()!='\n');
             break;
         case 't':
@@ -72,7 +82,7 @@ void exitHandler(int rien){
             getchar();
             break;
         case 'd':
-            printf("destination msk: %2x\nsource mask: %2x\n",dstMask,srcMask);
+            printf("destination mask: %2x\nsource mask: %2x\n",dstMask,srcMask);
             printf("type (displayed):\n");
             for (i=0;i<E_TYPE_COUNT;i++)  printf("%d %s   (%s)\n",i,eType2str(i),  ((dType[i]>0)?("yes"):("no")) );
             printf("press return\n");
@@ -84,6 +94,9 @@ void exitHandler(int rien){
         case 'q':
             printf("bye bye\n");
             Xbee_deInitSerial();
+            if(prevH){
+            	prevH(sig);
+            }
             exit(0);
             break;
         default:
@@ -95,7 +108,6 @@ void exitHandler(int rien){
     return;
 }
 
-
 int main(int argc, char *argv[]){
     sMsg inMsg;
     double bytesCount;
@@ -103,25 +115,24 @@ int main(int argc, char *argv[]){
     double elapsed;
     double byteRate;
 
-    //autorizing every type of message to be displayed:
+    // authorizing every type of message to be displayed:
     memset(dType,1,sizeof(dType));
 
     if(argc!=2){
-        printf("XBee sniffer & superBus network analyser\nMade by Quentin VEY for the ENAC robotic club, 2013\n\nusage: %s ttyport (ex: /dev/ttyUSB0)\n", argv[0]);
+        printf("XBee sniffer & superBus network analyzer\nMade by Quentin VEY for the ENAC robotic club, 2013\n\nusage: %s ttyport (ex: /dev/ttyUSB0)\n", argv[0]);
         return(1);
-     }
-        
-    sig_t prevH=signal(SIGINT,exitHandler);
+    }
+
+    prevH=signal(SIGINT,sigHandler);
 
     Xbee_initSerial(argv[1]);
 
     printf("ctrl+C to see the menu\n");
 
-
-    while ( bool ){
+    while ( 1 ){
         gettimeofday(&prevClock, NULL);
         if ( (bytesCount=Xbee_receive(&inMsg)) ){
-            if (  (!snetOnly || (inMsg.header.destAddr&SUBNET_MASK) ) && ((inMsg.header.destAddr & dstMask) || (inMsg.header.srcAddr & srcMask)) ) {
+            if ( (!snetOnly || (inMsg.header.destAddr&SUBNET_MASK) ) && ((inMsg.header.destAddr & dstMask) || (inMsg.header.srcAddr & srcMask)) ) {
                 switch (inMsg.header.type){
                 case E_SWITCH_CHANNEL:
                         if (dType[E_PERIOD] ){
@@ -170,12 +181,10 @@ int main(int argc, char *argv[]){
             }
             gettimeofday(&currentClock, NULL);
             elapsed = currentClock.tv_sec - prevClock.tv_sec + ((double) (currentClock.tv_usec - prevClock.tv_usec) )/ 1000000.;
-            byteRate=bytesCount/elapsed;
-            printf("\033[128D\033[50C%f o/s  ,%u\n",byteRate,clock());
+            byteRate = bytesCount/elapsed;
+            printf("\033[128D\033[50C%f o/s  ,%lu\n",byteRate,clock());
         }
     }
-    if(prevH) prevH(15);
-    printf(" adios\n");
-
+    // never reached
     return 0;
 }
