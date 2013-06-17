d50a1127b816ddb0957b7a93637916f081a0195e
Quentin V <vey.quentin@gmail.com>
2013-06-17 15:13:33 +0200

    [communication] [Xbee_api] Creates test project for Xbee API mode 2 on Arduino.

---
+++ communication/Xbee_API/Xbee_API_test_arduino/src/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: 17 juin 2013
+ *      Author: quentin
+ */
+
+#include "Arduino.h"
+#include "Xbee_API.h"
+
+
+void setup(){
+    serialInit(111111,0);
+
+}
+
+void loop(){
+
+}
---
+++ communication/Xbee_API/Xbee_API_test_arduino/src/params.h
@@ -0,0 +1,15 @@
+/*
+ * params.h
+ *
+ *  Created on: 17 juin 2013
+ *      Author: quentin
+ */
+
+#ifndef PARAMS_H_
+#define PARAMS_H_
+
+#define ARCH_328P_ARDUINO
+#define XBEE_WAITFRAME_TIMEOUT  1000000    // in microsecond
+#define XBEE_READBYTE_TIMEOUT   5000       // in microsecond
+
+#endif /* PARAMS_H_ */
