9c0bd9c57a681f2e250f442081b58e073f96bc33
Ludovic Lacoste <ludolacost@gmail.com>
2013-07-04 01:38:46 +0200

    [arduino/I²C_test] Fixes default eclipse config.

--- arch-arduino/tests/I2C_MULTIMASTER/src/params.h
+++ arch-arduino/tests/I2C_MULTIMASTER/src/params.h
@@ -19,9 +19,14 @@ typedef enum{
 //network config
 #define MYADDRI ADDRI_MAIN_TURRET
 #define MYADDRX ADDRX_MAIN
-#define SB_INC_MSG_BUF_SIZE 4
-#define ARCH_328P_ARDUINO
+#define MYADDRU 0
 
+#define SB_WAIT_SND_FAIL    50000
+#define SB_INC_MSG_BUF_SIZE 4
+#define XBEE_WAITFRAME_TIMEOUT 10
+#define XBEE_READBYTE_TIMEOUT 10000
+#define ARCH_328P_ARDUINO
+#define LITTLE_ENDIAN
 
 
 #define PIN_DBG_LED 13
