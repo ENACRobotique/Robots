83d2472d9337702fbf6a8e87c26c5c2ba99150b3
Ludovic Lacoste <ludolacost@gmail.com>
2013-12-17 02:51:56 +0100

    [LPC_hli2c_template] Converts lpc_hli2c_template to Eclipse. * Cleans template code.

--- static/core/lpc21xx/lpc2148/crt.s
+++ static/core/lpc21xx/lpc2148/crt.s
@@ -9,11 +9,11 @@
   *************************************************************************************************************** */
 
 /* Stack Sizes */
-.set  UND_STACK_SIZE, 0x00000010		/* stack for "undefined instruction" interrupts is 4 bytes  */
-.set  ABT_STACK_SIZE, 0x00000010		/* stack for "abort" interrupts is 4 bytes                  */
-.set  FIQ_STACK_SIZE, 0x00000010		/* stack for "FIQ" interrupts  is 4 bytes         			*/
-.set  IRQ_STACK_SIZE, 0X00000010		/* stack for "IRQ" normal interrupts is 4 bytes    			*/
-.set  SVC_STACK_SIZE, 0x00000010		/* stack for "SVC" supervisor mode is 4 bytes  				*/
+.set  UND_STACK_SIZE, 0x00000010		/* stack for "undefined instruction" interrupts is 16 bytes */
+.set  ABT_STACK_SIZE, 0x00000010		/* stack for "abort" interrupts is 16 bytes                 */
+.set  FIQ_STACK_SIZE, 0x00000010		/* stack for "FIQ" interrupts  is 16 bytes         			*/
+.set  IRQ_STACK_SIZE, 0X00000010		/* stack for "IRQ" normal interrupts is 16 bytes    		*/
+.set  SVC_STACK_SIZE, 0x00000010		/* stack for "SVC" supervisor mode is 16 bytes  			*/
 
 /* Standard definitions of Mode bits and Interrupt (I & F) flags in PSRs (program status registers) */
 .set  MODE_USR, 0x10            		/* Normal User Mode 										*/
--- static/core/tools/lpc_hli2c_template/main.c
+++ static/core/tools/lpc_hli2c_template/main.c
@@ -1,91 +1,86 @@
-#include <targets/LPC2000.h>
-#include <ctl_api.h>
-#include <math.h>
-#include <limits.h>
-#include <stdint.h>
+#include <lpc214x.h>
+#include <ime.h>
+#include <gpio.h>
+#include <sys_time.h>
+#include <params.h>
+#include <i2c.h>
 
-#include "gpio.h"
-#include "eint.h"
-#include "pwm.h"
-#include "sys_time.h"
-#include "params.h"
-#include "trigo.h"
-#include "i2c.h"
-
-#ifndef BIT
-#define BIT(b) (1<<(b))
-#endif
-
-#ifndef min
-#define min(a, b) ((a)>(b)?(b):(a))
-#endif
-#ifndef max
-#define max(a, b) ((a)>(b)?(a):(b))
-#endif
-#ifndef MINMAX
-#define MINMAX(m, v, M) max(m, min(v, M))
-#endif
-
-#define SQR(v) ((long long)(v)*(v))
-
-void mybreak_i() {
-  static int i = 0;
-  i++;
-}
-
-#define I2C_MASTER 2
+#define I2C_MASTER 1
+//#define I2C_POLL_PERIOD 200
 
 int main(void) {
-  static int led_etat = 0;
-  unsigned int prev_I2C = 0;
-  int ret;
-  char buffer[MAX_CHUNK_SIZE];
-  uint8_t addr;
+    unsigned int led0_status = 0, led1_status = 0;
+    unsigned int prev_led1 = 0;
+#ifdef I2C_POLL_PERIOD
+    unsigned int prev_I2C = 0;
+#endif
+    unsigned int time;
+    int ret;
+    uint8_t buffer[MAX_CHUNK_SIZE];
+    uint8_t addr;
 
-  gpio_init_all();  // use fast GPIOs
+    gpio_init_all();  // use fast GPIOs
 
 #if I2C_MASTER == 1
-  i2c_init(400000, 0x08<<1);
+    i2c_init(400000, 0x04<<1);
 #else
-  i2c_init(400000, 0x09<<1);
+    i2c_init(400000, 0x09<<1);
 #endif
 
-// sortie LED
-  gpio_output(1, 24);
-  gpio_write(1, 24, 0); // green LED on
+    // sortie LED
+    gpio_output(0, 31);
+    gpio_write(0, 31, 0); // orange LED on
 
-// init time management
-  sys_time_init();
+    gpio_output(1, 24);
+    gpio_write(1, 24, 0); // green LED on
 
-  ctl_global_interrupts_enable();
+    // init time management
+    sys_time_init();
+
+    global_IRQ_enable();
 
 #if I2C_MASTER == 1
-//  ret = i2c_sendchunk(0x09<<1, "Hello World!", 13);
+    //  ret = i2c_sendchunk(0x09<<1, "Hello World!", 13);
 #else
-  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
-//  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
-//  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
+    ret = i2c_sendchunk(0x04<<1, (uint8_t*)"Hello World!", 13);
+    //  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
+    //  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
 #endif
 
-// main loop
-  while(1) {
-    sys_time_update();
-
-//    if( millis() - prev_I2C > 2 ){
-//      prev_I2C = millis();
-
-      ret = i2c_recvchunk(&addr, buffer, sizeof(buffer));
-      if(ret > 0){
-        gpio_toggle(1, 24);
+    // main loop
+    while(1) {
+        sys_time_update();
+        time = millis();
 
 #if I2C_MASTER == 1
-        ret = i2c_sendchunk(0x09<<1, "Welcome", 40);
+        if( (time - prev_led1) > 100 )
 #else
-        ret = i2c_sendchunk(0x08<<1, "Welcome", 8);
+        if( (time - prev_led1) > 500 )
 #endif
-      }
-//    }
-  }
+        {
+            prev_led1 = time;
 
-  return 0;
+            gpio_write(0, 31, led1_status^=1);
+        }
+
+#ifdef I2C_POLL_PERIOD
+        if( (time - prev_I2C) > I2C_POLL_PERIOD ){
+            prev_I2C = time;
+#endif
+            ret = i2c_recvchunk(&addr, buffer, sizeof(buffer));
+            if(ret > 0){
+                gpio_write(1, 24, led0_status^=1);
+
+#if I2C_MASTER == 1
+//                ret = i2c_sendchunk(0x09<<1, (uint8_t*)"Welcome", 40);
+#else
+//                ret = i2c_sendchunk(0x08<<1, (uint8_t*)"Welcome", 8);
+#endif
+            }
+#ifdef I2C_POLL_PERIOD
+        }
+#endif
+    }
+
+    return 0;
 }
