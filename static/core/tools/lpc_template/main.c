cb3fb5508cabc907005dbe804b08877ae892d6b4
Ludovic Lacoste <ludolacost@gmail.com>
2014-01-16 23:10:51 +0100

    [lpc_template] Adds a basic test for PWM output.

--- static/core/tools/lpc_template/main.c
+++ static/core/tools/lpc_template/main.c
@@ -4,8 +4,14 @@
 #include <sys_time.h>
 #include <eint.h>
 #include <ime.h>
+#include <pwm.h>
+
+//#define TEST_EXTINT
+#define TEST_PWM
 
 volatile int status_flag = 0;
+
+#ifdef TEST_EXTINT
 volatile int irq_count = 0;
 
 void isr_eint0() __attribute__ ((interrupt("IRQ")));
@@ -25,6 +31,7 @@ void isr_eint3(){
     SCB_EXTINT = BIT(3); // acknowledges interrupt
     VIC_VectAddr = (unsigned)0; // updates priority hardware
 }
+#endif
 
 int	main(){
     unsigned int prevMillis = 0;
@@ -34,7 +41,15 @@ int	main(){
     gpio_init_all();
     sys_time_init();
 
+#ifdef TEST_EXTINT
     eint_init(isr_eint0 /* rising edge on P0.1 */, isr_eint3 /* rising edge on P0.20 */);
+#endif
+
+#ifdef TEST_PWM
+    pwm_init(0, 1024);
+
+    pwm_enable(4, 128);
+#endif
 
     gpio_output(1, 24);
     gpio_output(0, 31);
@@ -50,6 +65,10 @@ int	main(){
             prevMillis = time;
 
             state^=1; // toggle state
+
+#ifdef TEST_PWM
+            pwm_update(4, state?512:256);
+#endif
 
             switch(status_flag){
             default:
