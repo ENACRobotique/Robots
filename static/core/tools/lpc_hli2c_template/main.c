1d8dcfd0ee8f01bab67dbfed259aab34e9a9a00a
Ludovic Lacoste <ludolacost@gmail.com>
2014-01-22 00:28:01 +0100

    [lpc_hli2c_template] Cleanup

--- static/core/tools/lpc_hli2c_template/main.c
+++ static/core/tools/lpc_hli2c_template/main.c
@@ -5,15 +5,15 @@
 #include <sys_time.h>
 #include <i2c.h>
 
-#define I2C_MASTER 1 // must be equal to 1 or 2
-//#define I2C_POLL_PERIOD 200 // comment if necessary
+#define I2C_MASTER 2 // must be equal to 1 or 2
+
+#define I2CADDR_MASTER1 (0x08<<1)
+#define I2CADDR_MASTER2 (0x09<<1)
+#define I2CSPEED (400000)
 
 int main(void) {
     unsigned int led0_status = 0, led1_status = 0;
-    unsigned int prev_led1 = 0;
-#ifdef I2C_POLL_PERIOD
-    unsigned int prev_I2C = 0;
-#endif
+    unsigned int led1_prevtime = 0;
     unsigned int time;
     int ret;
     uint8_t buffer[MAX_CHUNK_SIZE];
@@ -22,17 +22,17 @@ int main(void) {
     gpio_init_all();  // use fast GPIOs
 
 #if I2C_MASTER == 1
-    i2c_init(400000, 0x04<<1);
+    i2c_init(I2CSPEED, I2CADDR_MASTER1);
 #else
-    i2c_init(400000, 0x09<<1);
+    i2c_init(I2CSPEED, I2CADDR_MASTER2);
 #endif
 
     // sortie LED
-    gpio_output(0, 31);
-    gpio_write(0, 31, 0); // orange LED on
-
     gpio_output(1, 24);
-    gpio_write(1, 24, 0); // green LED on
+    gpio_write(1, 24, led0_status); // green LED on when writing 0
+
+    gpio_output(0, 31);
+    gpio_write(0, 31, led1_status); // orange LED on when writing 0
 
     // init time management
     sys_time_init();
@@ -40,11 +40,11 @@ int main(void) {
     global_IRQ_enable();
 
 #if I2C_MASTER == 1
-    //  ret = i2c_sendchunk(0x09<<1, "Hello World!", 13);
+    //  ret = i2c_sendchunk(I2CADDR_MASTER2, "Hello World!", 13);
 #else
-    ret = i2c_sendchunk(0x04<<1, (uint8_t*)"Hello World!", 13);
-    //  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
-    //  ret = i2c_sendchunk(0x08<<1, "Hello World!", 13);
+//    ret = i2c_sendchunk(I2CADDR_MASTER1, (uint8_t*)"Hello World!", 13);
+    //  ret = i2c_sendchunk(I2CADDR_MASTER1, "Hello World!", 13);
+    //  ret = i2c_sendchunk(I2CADDR_MASTER1, "Hello World!", 13);
 #endif
 
     // main loop
@@ -52,34 +52,30 @@ int main(void) {
         sys_time_update();
         time = millis();
 
+        ret = i2c_recvchunk(&addr, buffer, sizeof(buffer));
+        if(ret > 0){
+            gpio_write(1, 24, buffer[0]);
+
 #if I2C_MASTER == 1
-        if( (time - prev_led1) > 100 )
+//            ret = i2c_sendchunk(I2CADDR_MASTER2, (uint8_t*)"Welcome", 40);
 #else
-        if( (time - prev_led1) > 500 )
+//            ret = i2c_sendchunk(I2CADDR_MASTER1, (uint8_t*)"Welcome", 8);
+#endif
+        }
+
+#if I2C_MASTER == 1
+        if( (time - led1_prevtime) > 100 )
+#else
+        if( (time - led1_prevtime) > 30 )
 #endif
         {
-            prev_led1 = time;
+            led1_prevtime = time;
 
             gpio_write(0, 31, led1_status^=1);
-        }
 
-#ifdef I2C_POLL_PERIOD
-        if( (time - prev_I2C) > I2C_POLL_PERIOD ){
-            prev_I2C = time;
-#endif
-            ret = i2c_recvchunk(&addr, buffer, sizeof(buffer));
-            if(ret > 0){
-                gpio_write(1, 24, led0_status^=1);
-
-#if I2C_MASTER == 1
-//                ret = i2c_sendchunk(0x09<<1, (uint8_t*)"Welcome", 40);
-#else
-//                ret = i2c_sendchunk(0x08<<1, (uint8_t*)"Welcome", 8);
-#endif
-            }
-#ifdef I2C_POLL_PERIOD
+            buffer[0] = led1_status;
+            i2c_sendchunk(I2CADDR_MASTER1, buffer, 1);
         }
-#endif
     }
 
     return 0;
