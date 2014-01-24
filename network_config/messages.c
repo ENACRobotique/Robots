5b68c7af98b50e2b2d8a684c3c059d3a18ed562e
Ludovic Lacoste <ludolacost@gmail.com>
2014-01-24 01:30:23 +0100

    [comm] Adds missing back-references to E_TYPE values

--- network_config/messages.c
+++ network_config/messages.c
@@ -9,18 +9,23 @@
 
 char *eType2str(E_TYPE elem){
     switch (elem){
-    case E_DEBUG : return "DEBUG";
-    case E_DEBUG_SIGNALLING : return "DEBUG_SIGNALLING";
-    case E_DATA : return "DATA";
+    case E_DEBUG :                  return "DEBUG";
+    case E_DEBUG_SIGNALLING :       return "DEBUG_SIGNALLING";
+    case E_DATA :                   return "DATA";
+    case E_ACK_RESPONSE :           return "ACK_RESPONSE";
+    case E_PING :                   return "PING";
+    case E_TRACEROUTE_REQUEST :     return "TRACEROUTE_REQUEST";
+    case E_TRACEROUTE_RESPONSE :    return "TRACEROUTE_RESPONSE";
 
     /************************ user types start ************************/
-    case E_SWITCH_CHANNEL : return "SWITCH_CHANNEL";
-    case E_SYNC_EXPECTED_TIME : return "SYNC_EXPECTED";
-    case E_SYNC_OK : return "SYNC_OK";
-    case E_PERIOD : return "PERIOD";
-    case E_MEASURE : return "MEASURE";
-    case E_TRAJ : return "TRAJ";
-    case E_POS : return "POS";
+    case E_SWITCH_CHANNEL :         return "SWITCH_CHANNEL";
+    case E_SYNC_EXPECTED_TIME :     return "SYNC_EXPECTED";
+    case E_SYNC_OK :                return "SYNC_OK";
+    case E_PERIOD :                 return "PERIOD";
+    case E_MEASURE :                return "MEASURE";
+    case E_TRAJ :                   return "TRAJ";
+    case E_POS :                    return "POS";
+    case E_SERIAL_DUMP :            return "SERIAL_DUMP";
     /************************ user types stop ************************/
 
     default :
@@ -29,3 +34,4 @@ char *eType2str(E_TYPE elem){
 
     return 0;
 }
+
