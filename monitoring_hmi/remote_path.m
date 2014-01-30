485a09a8e418e3f399647d34c60f45560be56ac9
Ludovic Lacoste <ludolacost@gmail.com>
2014-01-31 00:30:51 +0100

    [monitoring_hmi] Minor update to matlab tool

--- monitoring_hmi/remote_path.m
+++ monitoring_hmi/remote_path.m
@@ -5,9 +5,10 @@ x = 10; % (cm)
 y = -10; % (cm)
 theta = pi/4; % (rad)
 
-
-strt_x = x
-strt_y = y
+p1_x = x;
+p1_y = y;
+p2_x = p1_x;
+p2_y = p1_y;
 c_r = dist/angle
   l_x = c_r*sin(angle)
   l_y = c_r*(cos(angle)-1)
@@ -17,5 +18,7 @@ end_x = x + cos(theta)*l_x + -sin(theta)*l_y
 end_y = y + sin(theta)*l_x + cos(theta)*l_y
 
 figure;
-plot(strt_x, strt_y, '+k', c_x, c_y, '*b', end_x, end_y, '+r');
-axis('equal', [x-2*dist x+2*dist y-2*dist y+2*dist]);
+hold on;
+plot(p1_x, p1_y, '+g', p1_x+[0:dist/200:dist/5]*cos(theta), p1_y+[0:dist/200:dist/5]*sin(theta), '-b');
+plot(c_x, c_y, 'ob', end_x, end_y, '+r');
+axis('equal', [x-1.5*dist x+1.5*dist y-1.5*dist y+1.5*dist]);
