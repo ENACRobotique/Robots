235e0060812bf539baa9df2d328ee579ccb89bd0
Ludovic Lacoste <ludolacost@gmail.com>
2014-02-26 00:34:04 +0100

    [monitoring_hmi] Adds param. to choose between simulated and true prop * WIP: the simulated prop will be reachable at ADDRD_MAIN_PROP_SIMU(cherry picked from commit f286908d427da1a75a0a5c7c9a03938f0b0ff700)

--- monitoring_hmi/gtk.c
+++ monitoring_hmi/gtk.c
@@ -23,6 +23,7 @@ void usage(char *cl) {
     printf("GTK UI\n");
     printf("Usage:\n\t%s [options]\n", cl);
     printf("Options:\n");
+    printf("\t--prop-simulator, -s      uses prop simulator\n");
     printf("\t--verbose, -v             increases verbosity\n");
     printf("\t--quiet, -q               not verbose\n");
     printf("\t--help, -h, -?            prints this help\n");
@@ -84,7 +85,7 @@ int handle(GIOChannel *source, GIOCondition condition, context_t *ctx) {
 
             // send trajectory if event
             if(ctx->mouse_event){
-                outMsg.header.destAddr = ADDRI_MAIN_PROP;
+                outMsg.header.destAddr = ctx->prop_address;
                 outMsg.header.type = E_TRAJ;
                 outMsg.header.size = sizeof(outMsg.payload.traj);
                 // payload
@@ -175,8 +176,10 @@ int main(int argc, char *argv[]) {
 
     // arguments options
     ctx.verbose = 1;
+    ctx.prop_address = ADDRI_MAIN_PROP;
     while(1) {
         static struct option long_options[] = {
+            {"prop-simulator", no_argument,  NULL, 's'},
             {"verbose",   no_argument,       NULL, 'v'},
             {"quiet",     no_argument,       NULL, 'q'},
             {"help",      no_argument,       NULL, 'h'},
@@ -188,6 +191,9 @@ int main(int argc, char *argv[]) {
             break;
 
         switch(c) {
+        case 's':
+            ctx.prop_address = ADDRD_DBGBRIDGE;
+            break;
         case 'v':
             ctx.verbose++;
             break;
@@ -240,7 +246,7 @@ int main(int argc, char *argv[]) {
     // send position
     {
         sMsg outMsg;
-        outMsg.header.destAddr = ADDRI_MAIN_PROP;
+        outMsg.header.destAddr = ctx.prop_address;
         outMsg.header.type = E_POS;
         outMsg.header.size = sizeof(outMsg.payload.pos);
         outMsg.payload.pos.id = 0;
