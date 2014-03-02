04d57c45d8e1ff51cdb5a9cc9c6fb7c3a33a0691
Ludovic Lacoste <ludolacost@gmail.com>
2014-03-02 23:58:23 +0100

    [arduino_i2c_template] Project migrated to botNet, renamed to _bn_

---
+++ static/core/tools/arduino_bn_template/src/node_cfg.h
@@ -0,0 +1,26 @@
+/*
+ * node_cfg.h
+ */
+
+#ifndef NODE_CFG_H_
+#define NODE_CFG_H_
+
+#include "network_cfg.h"
+
+//network config
+#define MYADDRX 0
+#define MYADDRI ADDRI_MAIN_TURRET
+#define MYADDRU 0
+#define MYADDRD 0
+
+#define BN_INC_MSG_BUF_SIZE 4
+#define BN_WAIT_XBEE_SND_FAIL   25000
+#define BN_MAX_RETRIES          2
+#define BN_ACK_TIMEOUT          1000    //in ms
+
+#define UART_WAITFRAME_TIMEOUT 10
+#define UART_READBYTE_TIMEOUT 10000
+
+#define XBEE_RST_PIN 5
+
+#endif /* NODE_CFG_H_ */
