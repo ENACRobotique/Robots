ee7075a09556c4a04b69f15c5a8c71754823d774
Ludovic Lacoste <ludolacost@gmail.com>
2014-03-20 01:56:55 +0100

    [main_ia/A*] Adds some const qualifiers

--- primary_robot/main_ia/astar_tools/math_ops.h
+++ primary_robot/main_ia/astar_tools/math_ops.h
@@ -6,17 +6,17 @@
 #include "error.h"
 #include "math_types.h"
 
-inline ERROR normVec(sVec_t *v, sNum_t *n);
-inline ERROR convPts2Vec(sPt_t *a, sPt_t *b, sVec_t *ab);
-inline ERROR convPts2Seg(sPt_t *a, sPt_t *b, sSeg_t *ab);
-inline ERROR sqdistPt2Pt(sPt_t *p1, sPt_t *p2, sNum_t *d2);
-inline ERROR distPt2Pt(sPt_t *p1, sPt_t *p2, sNum_t *d);
-inline ERROR dotVecs(sVec_t *v1, sVec_t *v2, sNum_t *d);
-inline ERROR crossVecs(sVec_t *v1, sVec_t *v2, sNum_t *c);
-ERROR convVecPt2Line(sVec_t *v, sPt_t *p, int norm, sLin_t *l);
+inline ERROR normVec(const sVec_t *v, sNum_t *n);
+inline ERROR convPts2Vec(const sPt_t *a, const sPt_t *b, sVec_t *ab);
+inline ERROR convPts2Seg(const sPt_t *a, const sPt_t *b, sSeg_t *ab);
+inline ERROR sqdistPt2Pt(const sPt_t *p1, const sPt_t *p2, sNum_t *d2);
+inline ERROR distPt2Pt(const sPt_t *p1, const sPt_t *p2, sNum_t *d);
+inline ERROR dotVecs(const sVec_t *v1, const sVec_t *v2, sNum_t *d);
+inline ERROR crossVecs(const sVec_t *v1, const sVec_t *v2, sNum_t *c);
+ERROR convVecPt2Line(const sVec_t *v, const sPt_t *p, int norm, sLin_t *l);
 ERROR normLine(sLin_t *l);
-ERROR sqdistPt2Seg(sPt_t *p, sSeg_t *s, sNum_t *d, sPt_t *h);
-ERROR distPt2Line(sPt_t *p, sLin_t *l, sNum_t *d);
+ERROR sqdistPt2Seg(const sPt_t *p, const sSeg_t *s, sNum_t *d, sPt_t *h);
+ERROR distPt2Line(const sPt_t *p, sLin_t *l, sNum_t *d);
 
 static inline void dumpVec(sVec_t *v, char *name) {
     printf("%s(%.2f,%.2f)\n", name, v->x, v->y);
