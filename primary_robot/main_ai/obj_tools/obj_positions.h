8769ba74550b2b14a226534bc5829c4c339f71fc
Ludovic Lacoste <ludolacost@gmail.com>
2014-04-19 15:36:49 +0200

    [primary_robot] Continues positions dev...

--- primary_robot/main_ai/obj_tools/obj_positions.h
+++ primary_robot/main_ai/obj_tools/obj_positions.h
@@ -10,9 +10,12 @@
 
 // in µs
 #define SAME_DATE_THRESHOLD (1000)
+#define KEEP_OLD_THRESHOLD (5*1000*1000)
+#define PROP_ANSWER_TIMEOUT (2*1000*1000)
 
 sGenericPos *getLastPGPosition(eElement el);
 
 int received_new_generic_pos(sGenericPos *pos);
+void positions_maintenance();
 
 #endif /* OBJ_POSITION_H_ */
--- primary_robot/main_ai/obj_tools/obj_time_tools.c
+++ primary_robot/main_ai/obj_tools/obj_time_tools.c
@@ -5,11 +5,18 @@
  *      Author: ludo6431
  */
 
-
+#include <millis.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include "obj_time_tools.h"
 
+// overflow proof t1 - t2
 int32_t time_diff(uint32_t t1, uint32_t t2){
-    return (int32_t)(t1 - t2); // FIXME, works only if t1 more recent than t2
+    uint32_t time = micros() + 100000000UL; // FIXME, use synchronized micros() (to be sure <time> is more recent than <t1> and <t2>)
+    uint32_t dt1, dt2;
+
+    dt1 = time - t1;
+    dt2 = time - t2;
+
+    return dt1 > dt2 ? -(int32_t)(dt1 - dt2) : (int32_t)(dt2 - dt1);
 }
