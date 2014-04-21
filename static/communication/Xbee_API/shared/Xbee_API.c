8704da970be59d01d649d7eb280b86ea7417b46e
Ludovic Lacoste <ludolacost@gmail.com>
2014-04-21 02:29:37 +0200

    [XBee] Fixes some unreported errors

--- static/communication/Xbee_API/shared/Xbee_API.c
+++ static/communication/Xbee_API/shared/Xbee_API.c
@@ -146,9 +146,12 @@ int Xbee_waitATAck(int frID, uint32_t timeOut){
     //waits for acknowledgement
     do {
         byteRead=Xbee_readFrame(&stru);
+        if(byteRead < 0){
+            return byteRead;
+        }
     } while( !(byteRead>0 && stru.APID==XBEE_APID_ATRESPONSE && stru.data.TXStatus.frameID==frID) && testTimeout(timeOut,&sw));
 
-    if (byteRead<=0 || stru.APID!=XBEE_APID_ATRESPONSE || stru.data.TXStatus.frameID!=frID) return -ERR_XBEE_NOSTAT;
+    if (!byteRead || stru.APID!=XBEE_APID_ATRESPONSE || stru.data.TXStatus.frameID!=frID) return -ERR_XBEE_NOSTAT;
     else if (stru.data.ATResponse.status==XBEE_ATR_S_ERROR) return -ERR_XBEE_AT_ERR;
     else if (stru.data.ATResponse.status==XBEE_ATR_S_INVCOM) return -ERR_XBEE_AT_WRONG_CMD;
     else if (stru.data.ATResponse.status==XBEE_ATR_S_INVPAR) return -ERR_XBEE_AT_WRONG_PAR;
