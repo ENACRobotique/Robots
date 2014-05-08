32d1bb3ce54cb45df9e525efb4937fdddff1c274
SebRobot <sebastien.malissard@dbmail.com>
2014-05-08 17:16:56 +0200

    [main_ia] Fixes last comit in math_ops

--- primary_robot/main_ai/astar_tools/math_ops.c
+++ primary_robot/main_ai/astar_tools/math_ops.c
@@ -190,27 +190,30 @@ ERROR distPt2Line(const sPt_t *p, sLin_t *l, sNum_t *d, sPt_t *h) {
     return 0;
 }
 
-ERROR interLine2Line(const sLin_t *l1, const sLin_t *l2, int nb, sPt_t *pt){
+ERROR interLine2Line(const sLin_t *l1, const sLin_t *l2, int *nb, sPt_t *pt){
     float det;
 
-    RET_IF_NOT_(l1 && l2 && pt, ERR_BADPAR);
+    RET_IF_NOT_(l1 && l2 && nb, ERR_BADPAR);
 
     if(!(det = l1->a*l2->b - l1->b*l2->a)){ //parallel
-        nb = 0;
+        *nb = 0;
     }
     else{
-        pt->x = (1/det)*(l1->b*l2->c - l1->c*l2->b);
-        pt->y = (1/det)*(l1->c*l2->a - l1->a*l2->c);
-        nb = 1;
+        if(pt){
+            pt->x = (1/det)*(l1->b*l2->c - l1->c*l2->b);
+            pt->y = (1/det)*(l1->c*l2->a - l1->a*l2->c);
+        }
+        *nb = 1;
     }
 
     return 0;
 }
 
-ERROR interCircle2Line(const sLin_t *l, const sPt_t *c, sNum_t r, int nb, sPt_t *pt1, sPt_t *pt2){
-    float a, b, d, det;
+ERROR interCircle2Line(const sLin_t *l, const sPt_t *c, sNum_t r, int *nb, sPt_t *pt1, sPt_t *pt2){
+    sNum_t a, b, d, det;
+    sPt_t p1, p2;
 
-    RET_IF_NOT_(l && c && pt1 && pt2, ERR_BADPAR);
+    RET_IF_NOT_(l && c && nb, ERR_BADPAR);
     RET_IF_((l->a == 0) && (l->b ==0), ERR_BADPAR);
 
     if(l->a == 0){
@@ -218,95 +221,86 @@ ERROR interCircle2Line(const sLin_t *l, const sPt_t *c, sNum_t r, int nb, sPt_t 
         b = -2*c->x*l->b*l->b;
         d = l->b*l->b*(c->x*c->x + c->y*c->y - r*r) + 2*l->b*l->c*c->x + l->c*l->c;
         if( ((det = b*b - 4*a*d) < 0) || (b == 0) ){
-               nb = 0;
+               *nb = 0;
                return 0;
-               }
-       else{
-           pt1->x = (-b - sqrt(det))/(2*a);
-           pt1->y = -l->c/l->b;
-           pt2->x = (-b + sqrt(det))/(2*a);
-           pt2->y = -l->c/l->b;
-           }
-       }
+        }
+        else{
+            p1.x = (-b - sqrt(det))/(2*a);
+            p1.y = -l->c/l->b;
+            p2.x = (-b + sqrt(det))/(2*a);
+            p2.y = -l->c/l->b;
+        }
+    }
     else{
         a = (l->a*l->a + l->b*l->b);
         b = 2*(l->b*l->c + l->a*l->b*c->x - l->a*l->a*c->y);
         d = l->a*l->a*(c->x*c->x + c->y*c->y - r*r) + 2*l->a*l->c*c->x + l->c*l->c;
         if( (det = b*b - 4*a*d) < 0){
-            nb = 0;
+            *nb = 0;
             return 0;
-            }
+        }
         else{
-            pt1->y = (-b - sqrt(det))/(2*a);
-            pt1->x = -(l->c + l->b*pt1->y)/l->a;
-            pt2->y = (-b + sqrt(det))/(2*a);
-            pt2->x = -(l->c + l->b*pt2->y)/l->a;
-
-            }
+            p1.y = (-b - sqrt(det))/(2*a);
+            p1.x = -(l->c + l->b*p1.y)/l->a;
+            p2.y = (-b + sqrt(det))/(2*a);
+            p2.x = -(l->c + l->b*p2.y)/l->a;
         }
-
-    if( (pt1->x == pt2->x) && (pt1->y == pt2->y)){
-        nb = 1;
-        }
-    else{
-        nb = 2;
-        }
-
-    return 0;
     }
 
-ERROR testPtOnSeg(const sSeg_t *seg , const sPt_t *pt, int ret){
-    sNum_t d;
-
-    RET_IF_NOT_(seg && pt, ERR_BADPAR);
-
-    sqdistPt2Seg(pt, seg, &d, NULL);
-    if(d < 0.1){
-        ret = 1;
-        }
+    if( (p1.x == p2.x) && (p1.y == p2.y)){
+        *nb = 1;
+    }
     else{
-        ret = 0;
-        }
-
-    return 0;
+        *nb = 2;
     }
 
-ERROR testPtOnArc(const sPt_t *p1, const sPt_t *p2 ,const sPt_t *c, float r, sPt_t *p, int ret){
-    float theta1, theta2, theta;
+    if(pt1){
+        *pt1 = p1;
+    }
 
-    RET_IF_NOT_(p1 && p2 && c, ERR_BADPAR);
+    if(pt2){
+        *pt2 = p2;
+    }
+
+    return 0;
+}
+
+ERROR testPtOnArc(const sPt_t *p1, const sPt_t *p2 ,const sPt_t *c, sNum_t r, sPt_t *p, int *ret){ //test if the point belongs on the part of circle FIXME test function
+    sNum_t theta1, theta2, theta;
+
+    RET_IF_NOT_(p1 && p2 && c && p && ret, ERR_BADPAR);
 
     if( (theta1 = atan2(p1->y - c->y, p1->x - c->x)) < 0){
-        theta1 += M_PI;
-        }
+        theta1 += 2*M_PI;
+    }
     if( (theta2 = atan2(p2->y - c->y, p2->x - c->x)) < 0){
-        theta2 += M_PI;
-        }
+        theta2 += 2*M_PI;
+    }
     if( (theta = atan2(p->y - c->y, p->x - c->x)) < 0){
-        theta += M_PI;
-        }
+        theta += 2*M_PI;
+    }
 
     if(theta2 < theta1){
         if( (theta < theta1) && (theta > theta2) ){
-            ret = 0;
+            *ret = 0;
             return 0;
-            }
         }
+    }
     else{
         if( (theta < theta1) || (theta > theta2) ){
-            ret = 0;
+            *ret = 0;
             return 0;
-            }
         }
-
-    ret = 1;
-    return 0;
     }
+
+    *ret = 1;
+    return 0;
+}
 
 ERROR symPtprLine(const sPt_t *pt, sLin_t *l, sPt_t *ps){
     sPt_t pp;
 
-    RET_IF_NOT_(pt && l, ERR_BADPAR);
+    RET_IF_NOT_(pt && l && ps, ERR_BADPAR);
 
     distPt2Line(pt, l, NULL, &pp);
 
@@ -314,43 +308,38 @@ ERROR symPtprLine(const sPt_t *pt, sLin_t *l, sPt_t *ps){
     ps->y = pt->y - 2*(pt->y - pp.y);
 
     return 0;
-    }
+}
 
-ERROR testPtInZone(const sPt_t pz[], int nb, const sPt_t *pt, int ret){ //FIXME if angle between two line more than 180°
+ERROR testPtInZone(const sPt_t pz[], int nb, const sPt_t *pt, int *ret){ //FIXME if angle between two line more than 180°
     sLin_t l[nb];
     int sg_prev = 0, sg = 0;
     int i;
 
-    RET_IF_NOT_(pz && pt, ERR_BADPAR);
+    RET_IF_NOT_(pz && pt && nb, ERR_BADPAR);
 
     for( i = 0 ; i < nb ; i++){
         convPts2Line(&pz[i], &pz[(i+1)%(nb)], 0, &l[i]);
-        }
+    }
 
-    sign(l[0].a * pt->x + l[0].b * pt->y +l[0].c, sg_prev);
+    sign(l[0].a * pt->x + l[0].b * pt->y +l[0].c, &sg_prev);
 
     for( i = 1 ; i < nb ; i++){
-        sign( l[i].a * pt->x + l[i].b * pt->y +l[i].c, sg);
+        sign( l[i].a * pt->x + l[i].b * pt->y +l[i].c, &sg);
         if( sg != sg_prev){
-            ret = 0;
+            *ret = 0;
             return 0;
-            }
         }
-
-    ret = 1;
-    return 0;
     }
 
-ERROR sign(const float x, int sg){
+    *ret = 1;
+    return 0;
+}
 
-    RET_IF_NOT_(x, ERR_BADPAR);
+ERROR sign(const float x, int *sg){
 
-    if(x==0){
-        sg = 0;
-        return 0;
-        }
+    RET_IF_NOT_(sg, ERR_BADPAR);
 
-    sg = (int) (x/fabs(x));
+    *sg = (x > 0) - (x < 0);
 
     return 0;
-    }
+}
