383117caa2b206ff19a857be39f3d71efce39e83
Sébastien Malissard <sebastien.malissard@dbmail.com>
2014-05-19 23:24:11 +0200

    [main_ai] Adds camera functions for fire,  unfinished

---
+++ primary_robot/main_ai/obj_tools/obj_camera.h
@@ -0,0 +1,18 @@
+/*
+ * obj_camera.h
+ *
+ *  Created on: 3 mai 2014
+ *      Author: seb
+ */
+
+#ifndef OBJ_CAMERA_H_
+#define OBJ_CAMERA_H_
+
+#define PRES_CAM_FIRE 5
+#define NB_FIRE 16
+
+#include "math_types.h"
+
+void getStatusFire(void);
+
+#endif /* OBJ_CAMERA_H_ */
