582e7b81d2129e37ecd4b939eef65b4135768b5b
Quentin Vey <vey.quentin@gmail.com>
2014-05-25 18:07:20 +0200

    [localization] Defines some types for nelder-mead interface with laser.

--- static/localization/balise_fixe/src/perception.h
+++ static/localization/balise_fixe/src/perception.h
@@ -3,6 +3,14 @@
 
 #include "tools.h"
 
+typedef enum {
+    BEACON_1=0,
+    BEACON_2,
+    BEACON_3,
+
+    BEACON_AMOUNT
+}eBeacon;   // beacon identifier
+
 typedef struct {
 // distances aux balises (m)
     float d1;
@@ -22,6 +30,16 @@ typedef struct {
     float u_a23;
     float u_a31;
 } sPerception;
+
+typedef struct {
+    unsigned long deltaT;       // µs, delay between two laser small peaks
+    unsigned long date;         // local µs, when was the laser recorded last
+    unsigned long period;       // µs, MEASURED period (0 if not applicable).
+    unsigned long u_date;
+    eBeacon beacon;             // beacon identifier
+}sMeasures;
+
+sPerception calcPerception( sMeasures *mes1, sMeasures *mes2, sMeasures *mes3);
 
 void simu_perception(sPt_t *x, sPerception *p);
 void estim_incertitude(sPerception *p);
