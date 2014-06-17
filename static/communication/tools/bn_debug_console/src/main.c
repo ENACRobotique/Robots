73c3e539b37d4704e82ad69bb64c18e366250fa6
Ludovic Lacoste <ludolacost@gmail.com>
2014-06-17 23:25:35 +0200

    [bn_debug_console] Adds commands to sync and test pos_history

--- static/communication/tools/bn_debug_console/src/main.c
+++ static/communication/tools/bn_debug_console/src/main.c
@@ -18,6 +18,7 @@
 
 #include "../botNet/shared/botNet_core.h"
 #include "../network_tools/bn_debug.h"
+#include "../network_tools/bn_intp.h"
 #include "../network_tools/bn_utils.h"
 #include "../../global_errors.h"
 #include "../../core/linux/libraries/Millis/millis.h"
@@ -134,6 +135,9 @@ int main(int argc, char **argv){
                 if(fd) fprintf(fd,"message received from %hx, type : %s (%hhu), seq : %02hhu ",msgIn.header.srcAddr,eType2str(msgIn.header.type),msgIn.header.type, msgIn.header.seqNum);
             }
             switch (msgIn.header.type){
+            case E_GENERIC_STATUS:
+                printf("%.2fcm, %.2fcm, %.2f°\n", msgIn.payload.genericStatus.prop_status.pos.x, msgIn.payload.genericStatus.prop_status.pos.y, msgIn.payload.genericStatus.prop_status.pos.theta*180./M_PI);
+                break;
             case E_ASSERV_STATS :
                 {
                     int i;
@@ -302,6 +306,26 @@ int main(int argc, char **argv){
                     msg.payload.servos.servos[0].us = us;
 
                     bn_send(&msg);
+                    break;
+                }
+                case 'h':
+                    printf("Syncing propulsion..."); fflush(stdout);
+                    bn_intp_sync(role_get_addr(ROLE_PROPULSION), 100);
+                    printf("done\n");
+                    break;
+                case 'w':{
+                    sMsg msg = {{0}};
+
+                    msg.header.destAddr = role_get_addr(ROLE_PROPULSION);
+                    msg.header.type = E_POS_QUERY;
+                    msg.header.size = sizeof(msg.payload.posQuery);
+
+                    msg.payload.posQuery.date = (int32_t)micros() - 200000;
+                    msg.payload.posQuery.id = ELT_PRIMARY;
+
+                    bn_send(&msg);
+
+                    printf("Sent position query\n");
                     break;
                 }
                 case 'd':{ // sends new setpoint to the propulsion // FIXME: use sGenericStatus
