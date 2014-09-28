8ac90f7e9954fd682b5b28c385a2a26e92c06f35
Ludovic Lacoste <ludolacost@gmail.com>
2014-09-28 19:11:26 +0200

    [tests/Yaml] Updates exporter, errors checking

--- static/tests/Yaml/exporter.c
+++ static/tests/Yaml/exporter.c
@@ -12,88 +12,96 @@
 #include <malloc.h>
 
 int main() {
-    FILE *fd_traj = fopen("traj.yml", "wb+");
     char tab[64];
     yaml_emitter_t traj_emitter;
     yaml_event_t traj_event;
     int i;
 
-    assert(yaml_emitter_initialize(&traj_emitter));
+#define ERR_CHECK(ret) do { if(!(ret)){ printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error); } } while(0)
+
+    FILE *fd_traj = fopen("traj.yml", "wb+");
+    if(!fd_traj){
+        perror("fopen");
+        exit(1);
+    }
+
+    // initialize the writer, object used in each call
+    ERR_CHECK(yaml_emitter_initialize(&traj_emitter));
     yaml_emitter_set_output_file(&traj_emitter, fd_traj);
 
-    assert(yaml_stream_start_event_initialize(&traj_event, YAML_UTF8_ENCODING));
-    assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+    // start document
+    ERR_CHECK(yaml_stream_start_event_initialize(&traj_event, YAML_UTF8_ENCODING));
+    ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
     // only one node inside a document
-    assert(yaml_document_start_event_initialize(&traj_event, NULL, NULL, NULL, 1));
-    assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+    ERR_CHECK(yaml_document_start_event_initialize(&traj_event, NULL, NULL, NULL, 1));
+    ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
     {
-        assert(yaml_sequence_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_SEQUENCE_STYLE));
-        assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+        // starting a list
+        ERR_CHECK(yaml_sequence_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_SEQUENCE_STYLE));
+        ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
         {
-            // dictionnary / block mapping node
-            assert(yaml_mapping_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_MAPPING_STYLE));
-            assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+            // first element of the list
+            // mapping node
+            ERR_CHECK(yaml_mapping_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_MAPPING_STYLE));
+            ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
             for (i = 0; i < 10; i++) {
                 sprintf(tab, "sid:%i", i);
 
-                yaml_scalar_event_initialize(&traj_event, NULL, NULL, "clef", 4, 1, 0, YAML_PLAIN_SCALAR_STYLE);
-                if (!yaml_emitter_emit(&traj_emitter, &traj_event)) {
-                    printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error);
-                }
+                // key
+                ERR_CHECK(yaml_scalar_event_initialize(&traj_event, NULL, NULL, "clef", 4, 1, 0, YAML_PLAIN_SCALAR_STYLE));
+                ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
-                yaml_scalar_event_initialize(&traj_event, NULL, NULL, tab, strlen(tab), 1, 0, YAML_PLAIN_SCALAR_STYLE);
-                if (!yaml_emitter_emit(&traj_emitter, &traj_event)) {
-                    printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error);
-                }
+                // value
+                ERR_CHECK(yaml_scalar_event_initialize(&traj_event, NULL, NULL, tab, strlen(tab), 1, 0, YAML_PLAIN_SCALAR_STYLE));
+                ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
             }
-            assert(yaml_mapping_end_event_initialize(&traj_event));
-            assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+            ERR_CHECK(yaml_mapping_end_event_initialize(&traj_event));
+            ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
-            assert(yaml_mapping_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_MAPPING_STYLE));
-            assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+            // second element of the list
+            // mapping node
+            ERR_CHECK(yaml_mapping_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_MAPPING_STYLE));
+            ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
             {
-                yaml_scalar_event_initialize(&traj_event, NULL, NULL, "clef", 4, 1, 0, YAML_PLAIN_SCALAR_STYLE);
-                if (!yaml_emitter_emit(&traj_emitter, &traj_event)) {
-                    printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error);
-                }
+                // key
+                ERR_CHECK(yaml_scalar_event_initialize(&traj_event, NULL, NULL, "clef", 4, 1, 0, YAML_PLAIN_SCALAR_STYLE));
+                ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
+                // value
                 // list / sequence
-                assert(yaml_sequence_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_SEQUENCE_STYLE));
-                assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+                ERR_CHECK(yaml_sequence_start_event_initialize(&traj_event, NULL, NULL, 1, YAML_BLOCK_SEQUENCE_STYLE));
+                ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
                 for (i = 0; i < 10; i++) {
                     sprintf(tab, "sid:%i", i);
 
-                    yaml_scalar_event_initialize(&traj_event, NULL, NULL, tab, strlen(tab), 1, 0, YAML_PLAIN_SCALAR_STYLE);
-                    if (!yaml_emitter_emit(&traj_emitter, &traj_event)) {
-                        printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error);
-                    }
+                    ERR_CHECK(yaml_scalar_event_initialize(&traj_event, NULL, NULL, tab, strlen(tab), 1, 0, YAML_PLAIN_SCALAR_STYLE));
+                    ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
                 }
-                assert(yaml_sequence_end_event_initialize(&traj_event));
-                assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+                ERR_CHECK(yaml_sequence_end_event_initialize(&traj_event));
+                ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
             }
-            assert(yaml_mapping_end_event_initialize(&traj_event));
-            assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+            ERR_CHECK(yaml_mapping_end_event_initialize(&traj_event));
+            ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
         }
-        assert(yaml_sequence_end_event_initialize(&traj_event));
-        assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+        ERR_CHECK(yaml_sequence_end_event_initialize(&traj_event));
+        ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
     }
-    assert(yaml_document_end_event_initialize(&traj_event, 1));
-    assert(yaml_emitter_emit(&traj_emitter, &traj_event));
+    // end of document
+    ERR_CHECK(yaml_document_end_event_initialize(&traj_event, 1));
+    ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
-    assert(yaml_stream_end_event_initialize(&traj_event));
-    if (!yaml_emitter_emit(&traj_emitter, &traj_event)) {
-        printf("error %s (%i)\n", traj_emitter.problem, traj_emitter.error);
-    }
+    ERR_CHECK(yaml_stream_end_event_initialize(&traj_event));
+    ERR_CHECK(yaml_emitter_emit(&traj_emitter, &traj_event));
 
-    assert(yaml_emitter_flush(&traj_emitter));
+    ERR_CHECK(yaml_emitter_flush(&traj_emitter));
 
     yaml_emitter_delete(&traj_emitter);
-
-    if (fd_traj)
-        fclose(fd_traj);
+    fclose(fd_traj);
 
     malloc_stats();
 
     return 0;
+
+#undef ERR_CHECK
 }
