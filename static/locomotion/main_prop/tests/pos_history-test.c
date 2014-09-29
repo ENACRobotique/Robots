d911a43dae7f1d853405a22cf587e00e63869e4c
Ludovic Lacoste <ludolacost@gmail.com>
2014-09-30 01:12:46 +0200

    [pos_history] Minor updates for pos_history test

--- static/locomotion/main_prop/tests/pos_history-test.c
+++ static/locomotion/main_prop/tests/pos_history-test.c
@@ -18,10 +18,10 @@
 
 int main(int argc, char *argv[]){
     sPeriod p = TP_Ms_CTOR(20); // 20 ms period
-    sDate start = TD_LoUs_CTOR(UINT32_MAX - 700500), end; // start defined so that the date overflows
+    sDate start = TD_LoUs_CTOR(UINT32_MAX - 700500); // start defined so that the date overflows
     const sPeriod ph_MEM_PERIOD = TP_Ms_CTOR(PH_MEMORY_PERIOD);
 
-    sDate d;
+    sDate d, startMem, endMem;
     sGenericStatus s;
     int i;
 
@@ -58,14 +58,14 @@ int main(int argc, char *argv[]){
         ph_enqueue(&s, d);
 
         if(i == NB_STEPS - 1){
-            end = d;
+            endMem = d;
         }
     }
 
     // try to query position in the past
     d = tD_addUs(start, 3000); // force desynchro
 
-    start = tD_subPeriod(end, ph_MEM_PERIOD); // we do not store a position before end
+    startMem = tD_subPeriod(endMem, ph_MEM_PERIOD); // we only store a position between start and endMem
 
     for(i = 0; i < NB_STEPS; d = tD_addPeriod(tD_addUs(d, (rand()&1023)-512), p), i++){
         printf("d:%.3fs\n", (float)TD_GET_LoUs(d)/1000000.);
@@ -74,13 +74,13 @@ int main(int argc, char *argv[]){
             printf("\tcan't find position!\n");
 
             // assert out of range of memory
-            assert(TD_DIFF_Us(d, start) < 0 || TD_DIFF_Us(d, end) > 0);
+            assert(TD_DIFF_Us(d, start) < 0 || TD_DIFF_Us(d, endMem) > 0);
 
             continue;
         }
 
         // assert inside range of memory
-        assert(TD_DIFF_Us(d, start) >= 0 && TD_DIFF_Us(d, end) <= 0);
+        assert(TD_DIFF_Us(d, start) >= 0 && TD_DIFF_Us(d, endMem) <= 0);
 
 #ifdef OUT
         fprintf(out, "%u;%.1f;%.1f;%.1f\n", TD_GET_LoUs(d), s.prop_status.pos.x, s.prop_status.pos.y, s.prop_status.pos.theta * 180. / M_PI);
