6884ccb5b5b0b2a692fa5b83c28c7c4c905e2989
Ludovic Lacoste <ludolacost@gmail.com>
2014-10-07 23:35:58 +0200

    [cairo_hmi] Adds playground drawing (with tranformation to fit in window) * Adds global context to store "global" data

--- static/tests/test_cairo_hmi/test_cairo_hmi.c
+++ static/tests/test_cairo_hmi/test_cairo_hmi.c
@@ -15,50 +15,102 @@
 #include <math.h>
 #include "millis.h"
 
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-    cairo_set_source_rgb(cr, 0.5+0.5*cos((double)millis()/250.), 0, 0);
-    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_font_size(cr, 40.0 + 5.*sin((double)millis()/150.));
+typedef struct {
+    GtkWidget *window;
+    GtkWidget *drawing_area;
+    gint da_width, da_height;
 
-    cairo_move_to(cr, 10.0, 50.0 + 5.*cos((double)millis()/200.));
-    cairo_show_text(cr, "Disziplin ist Macht.");
+    double wld_width, wld_height;
+} sContext;
+
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, sContext *ctx) {
+    { // prepare transformation for playground to fit in window not stretched
+        ctx->da_width = gtk_widget_get_allocated_width(ctx->drawing_area);
+        ctx->da_height = gtk_widget_get_allocated_height(ctx->drawing_area);
+
+        double scale = (double) ctx->da_height / ctx->wld_height;
+        double w = scale * ctx->wld_width, h;
+        if (w < ctx->da_width) {
+            cairo_translate(cr, ((double) ctx->da_width - w) / 2., 0.);
+        }
+        else {
+            scale = (double) ctx->da_width / ctx->wld_width;
+            h = scale * ctx->wld_height;
+            cairo_translate(cr, 0., ((double) ctx->da_height - h) / 2.);
+        }
+        cairo_scale(cr, scale, scale);
+        cairo_translate(cr, 0, ctx->wld_height);
+        cairo_scale(cr, 1, -1);
+    }
+
+    { // this is where we draw in centimeters!
+      // draw x axis
+        cairo_set_source_rgb(cr, 1, 0, 0);
+        cairo_move_to(cr, 0, 0);
+        cairo_line_to(cr, 10, 0);
+        cairo_stroke(cr);
+
+        // draw y axis
+        cairo_set_source_rgb(cr, 0, 1, 0);
+        cairo_move_to(cr, 0, 0);
+        cairo_line_to(cr, 0, 10);
+        cairo_stroke(cr);
+
+        // draw playground outline
+        double x = 1.5, y = 1.5;
+        cairo_device_to_user_distance(cr, &x, &y);
+        cairo_set_line_width(cr, MAX(x, y));
+        cairo_set_source_rgba(cr, 0, 0, 0, 0.5);
+        cairo_rectangle(cr, 0., 0., ctx->wld_width, ctx->wld_height);
+        cairo_stroke(cr);
+
+        // draw some moving text
+        cairo_move_to(cr, 100.0, 50.0 + 5. * cos((double) millis() / 200.));
+        cairo_save(cr);
+        cairo_scale(cr, 1, -1);
+        cairo_set_source_rgb(cr, 0.5 + 0.5 * cos((double) millis() / 250.), 0, 0);
+        cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+        cairo_set_font_size(cr, 40.0 + 5. * sin((double) millis() / 150.));
+        cairo_show_text(cr, "Disziplin ist Macht.");
+        cairo_restore(cr);
+    }
 
     return FALSE;
 }
 
-gint invalidate(GtkWidget *darea) {
-    gint width, height;
-    width = gtk_widget_get_allocated_width(darea);
-    height = gtk_widget_get_allocated_height(darea);
+gboolean invalidate(sContext *ctx) {
+    // for now, invalidate all
+    GdkRectangle rect = { .x = 0, .y = 0, .width = ctx->da_width, .height = ctx->da_height };
 
-    GdkRectangle rect = { .x = 0, .y = 0, .width = width, .height = height };
+    gdk_window_invalidate_rect(gtk_widget_get_window(ctx->drawing_area), &rect, 1);
 
-    gdk_window_invalidate_rect(gtk_widget_get_window(darea), &rect, 1);
-
-    return 1; // continue to be called
+    return TRUE; // continue to be called
 }
 
 int main(int argc, char *argv[]) {
-    GtkWidget *window;
-    GtkWidget *darea;
+    sContext ctx;
+
+    // cairo coordinates will be managed in centimeters
+    ctx.wld_width = 300.;
+    ctx.wld_height = 200.;
 
     gtk_init(&argc, &argv);
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    ctx.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
-    darea = gtk_drawing_area_new();
-    gtk_container_add(GTK_CONTAINER(window), darea);
+    ctx.drawing_area = gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(ctx.window), ctx.drawing_area);
 
-    g_signal_connect(G_OBJECT(darea), "draw", G_CALLBACK(on_draw_event), NULL);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(ctx.drawing_area), "draw", G_CALLBACK(on_draw_event), &ctx);
+    g_signal_connect(ctx.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 90);
-    gtk_window_set_title(GTK_WINDOW(window), "GTK window");
+    gtk_window_set_position(GTK_WINDOW(ctx.window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(ctx.window), 400, 90);
+    gtk_window_set_title(GTK_WINDOW(ctx.window), "GTK window");
 
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(ctx.window);
 
-    g_timeout_add(1000/30, (GSourceFunc)invalidate, darea); // 30Hz
+    g_timeout_add(1000 / 25, (GSourceFunc) invalidate, &ctx); // 25Hz
 
     gtk_main();
 
