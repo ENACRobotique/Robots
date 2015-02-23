b1817cc2f62d9eba8a6bd1e59959909939a92dc0
EngelOfChipolata <buisanguilhem@gmail.com>
2015-02-23 20:11:38 +0100

    First commit

---
+++ secondary_robot/src/states/state_stairs.cpp
@@ -0,0 +1,21 @@
+/*
+ * state_stairs.cpp
+ *
+ *  Created on: 23 févr. 2015
+ *      Author: guilhem
+ */
+
+#include "Arduino.h"
+#include "state_types.h"
+#include "lib_move.h"
+#include "lib_attitude.h"
+#include "lib_fan.h"
+#include "../tools.h"
+#include "../params.h"
+#include "state_traj.h"
+#include "state_pause.h"
+#include "state_wall.h"
+#include "state_wait.h"
+#include "state_lineMonit.h"
+
+
---
+++ secondary_robot/src/states/state_stairs.h
@@ -0,0 +1,15 @@
+/*
+ * state_stairs.h
+ *
+ *  Created on: 23 févr. 2015
+ *      Author: guilhem
+ */
+
+#ifndef SRC_STATES_STATE_STAIRS_H_
+#define SRC_STATES_STATE_STAIRS_H_
+
+
+
+
+
+#endif /* SRC_STATES_STATE_STAIRS_H_ */
