b3357df0a568ce92f21c63b5ca28b826d6353cc5
Ludovic Lacoste <ludolacost@gmail.com>
2015-02-24 19:57:51 +0100

    [network_config] Adds new message type for complex holo trajectories ; Removes new-old one

--- network_config/messages-locomotion.h
+++ network_config/messages-locomotion.h
@@ -14,6 +14,7 @@ typedef struct{
     float speed;
 } sSpeedSetPoint;
 
+// deprecated, use sTrajOrientElRaw_t
 typedef struct {
 // segment (centimeters)
     float p1_x;
@@ -29,19 +30,43 @@ typedef struct {
 // trajectory data
     uint16_t tid; // trajectory identifier
     uint16_t sid; // step identifier
-    float cumul_cvdist; // cumulative curvilinear distance before this step (centimeters)
 } sTrajElRaw_t;
 
 typedef struct {
-// start point
-    float p1_s; // traveled distance (aka curvilinear abscissa)
-    float p1_a; // orientation (rad)
-    float seg_cvlen; // curvilinear length of this orientation step
-// orientation data
-    uint16_t tid; // orientation identifier
-    uint16_t sid; // step identifier
-    float cumul_cvdist; // cumulative curvilinear distance before this step (centimeters)
-} sOrientElRaw_t;
+	// time constraints
+	uint32_t t1; // time at elts[0].p1 (synchronized time in us)
+	uint16_t t2; // time at elts[0].p2 (offset wrt t1 in ms)
+	uint16_t t3; // time at elts[1].p1 (offset wrt t2 in ms)
+	uint16_t t4; // time at elts[1].p2 (offset wrt t3 in ms)
+
+	// full trajectory data
+    uint16_t tid :12; // trajectory identifier (4096 trajectory id-s)
+    uint16_t sid :4; // step identifier (may loop)
+
+	struct __attribute__((packed)) {
+	// trajectory
+		// segment
+		int16_t p1_x; // cm<<6
+		int16_t p1_y; // cm<<6
+		int16_t p2_x; // cm<<6
+		int16_t p2_y; // cm<<6
+
+		// circle arc
+		int16_t c_x; // cm<<6
+		int16_t c_y; // cm<<6
+		int16_t c_r; // cm<<6 (>0 CW | <0 CCW)
+
+	// orientation
+		int16_t theta1; // rad<<13
+		int16_t theta2; // rad<<13
+
+	// extra packed data
+		int16_t seg_len :15; // cm<<5
+		int16_t rot1_dir :1; // sign bit for the rotation 1 direction (0: CW | 1: CCW)
+		int16_t arc_len :15; // cm<<5
+		int16_t rot2_dir :1; // sign bit for the rotation 2 direction (0: CW | 1: CCW)
+	} elts[2]; // 22x2 bytes
+} sTrajOrientElRaw_t; // exactly 56 bytes
 
 #define NB_ASSERV_STEPS_PER_MSG (4)
 typedef struct __attribute__((packed)){
