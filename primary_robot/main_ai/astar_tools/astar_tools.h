79626cafae6ede9c1d5443c50cd975e626a9037d
Sébastien Malissard <sebastien.malissard@dbmail.com>
2015-03-04 21:20:38 +0100

    [main_ai] New elements : Robomovies 2015 !!!

--- primary_robot/main_ai/astar_tools/astar_tools.h
+++ primary_robot/main_ai/astar_tools/astar_tools.h
@@ -120,7 +120,7 @@ typedef struct __attribute__((packed)){ // XXX this attribute reduces the size o
 
 // number of physical obstacles (16)
 #if 1
-#define N (13)
+#define N (41)
 #else
 #define N (54)
 #endif
