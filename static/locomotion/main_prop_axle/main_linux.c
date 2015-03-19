4c67a1b40982625d4b42f5889e163a47507c7c18
Ludovic Lacoste <ludolacost@gmail.com>
2015-03-19 22:21:16 +0100

    [main_prop_axle] Fixes type passed to new_traj_orient_el

--- static/locomotion/main_prop_axle/main_linux.c
+++ static/locomotion/main_prop_axle/main_linux.c
@@ -177,10 +177,10 @@ int main(int argc, char *argv[]){
                 }
                 break;
             case E_TRAJ_ORIENT_EL:
-                ret = new_traj_orient_el(&inMsg.payload.traj);
-                    if(ret < 0){
-                        printf("new_traj_el_orient() error #%i\n", -ret);
-                    }
+                ret = new_traj_orient_el(&inMsg.payload.trajOrientEl);
+                if(ret < 0){
+                    printf("new_traj_el_orient() error #%i\n", -ret);
+                }
                 break;
             case E_POS:
                 bn_printDbg("got pos");
