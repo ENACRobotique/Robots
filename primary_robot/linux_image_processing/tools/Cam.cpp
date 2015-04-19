f6797f1d6cb82721cb43e66406ee98e8b68b42d4
YoannSolana <ysolana@laas.fr>
2015-04-19 20:56:34 +0200

    [Image processing] Complet class Cam

--- primary_robot/linux_image_processing/tools/Cam.cpp
+++ primary_robot/linux_image_processing/tools/Cam.cpp
@@ -1,21 +1,23 @@
 #include "Cam.h"
 
-Cam::Cam(float f, Vector2D<float> size, PosObj3D<float> posCam){
+Cam::Cam(float f, Vector2D<float> size, ConfObj3D<float> confCam){
+    conf = confCam;
+    aperAngl = atan2(size.y/2, f);
     // Construct transition matrix from image to camera
-    K_C2I = (cv::Mat_<float>(3,3) << f, 0, size.x,
-                                 0, f, size.y,
-                                 0, 0, 1);
+    K_C2I = (cv::Mat_<float>(3,3) << f, 0, (size.x-1)/2,
+                                     0, f, (size.y-1)/2,
+                                     0, 0,  1);
 
     // Construct the transition matrix from camera to image
     K_C2I = K_I2C.inv();
 
     // construct the rotation matrix from robot to camera
-    float crx = cos(posCam.rx);
-    float srx = sin(posCam.rx);
-    float cry = cos(posCam.ry);
-    float sry = sin(posCam.ry);
-    float crz = cos(posCam.rz);
-    float srz = sin(posCam.rz);
+    float crx = cos(conf.rx);
+    float srx = sin(conf.rx);
+    float cry = cos(conf.ry);
+    float sry = sin(conf.ry);
+    float crz = cos(conf.rz);
+    float srz = sin(conf.rz);
     cv::Mat rotX_R2C = (cv::Mat_<float>(3,3) <<   1,   0,     0,
                                                   0,  crx,  srx,
                                                   0, -srx,  crx);
@@ -27,22 +29,25 @@ Cam::Cam(float f, Vector2D<float> size, PosObj3D<float> posCam){
                                                   0,    0,    1);
     cv::Mat rot_R2C = rotX_R2C*rotY_R2C*rotZ_R2C;
 
-    // Construct the translation matrix from robot to camera
-    cv::Mat trsl_R2C = (cv::Mat_<float>(3,1) << posCam.x,
-                                                posCam.y,
-                                                posCam.z);
+    // Construct the rotation matrix from camera to robot
+    cv::Mat rot_C2R = rot_R2C.inv();
 
-    // Construct the transition matrix from robot 2 camera
-    cv::Mat Transi_R2C = (cv::Mat_<float>(4,4));
-    cv::Mat Z = (cv::Mat_<float>(1,3) << 0,0,0);
-    cv::Mat One = (cv::Mat_<float>(1,1) << 1);
-    rot_R2C.copyTo(Transi_R2C(cv::Rect(0,0,rot_R2C.cols, rot_R2C.rows)));
-    trsl_R2C.copyTo(Transi_R2C(cv::Rect(3,0,trsl_R2C.cols, trsl_R2C.rows)));
-    Z.copyTo(Transi_R2C(cv::Rect(0,3,Z.cols, Z.rows)));
-    One.copyTo(Transi_R2C(cv::Rect(3,3,One.cols, One.rows)));
+    // Construct the translation matrix from camera to robot
+    cv::Mat trsl_C2R = (cv::Mat_<float>(3,1) << conf.x,
+                                                conf.y,
+                                                conf.z);
 
     // Construct the transition matrix from camera to robot
-    Transi_C2R = Transi_R2C.inv();
+    cv::Mat Transi_C2R = (cv::Mat_<float>(4,4));
+    cv::Mat Z = (cv::Mat_<float>(1,3) << 0,0,0);
+    cv::Mat One = (cv::Mat_<float>(1,1) << 1);
+    rot_C2R.copyTo(Transi_C2R(cv::Rect(0,0,rot_C2R.cols, rot_C2R.rows)));
+    trsl_C2R.copyTo(Transi_C2R(cv::Rect(3,0,trsl_C2R.cols, trsl_C2R.rows)));
+    Z.copyTo(Transi_C2R(cv::Rect(0,3,Z.cols, Z.rows)));
+    One.copyTo(Transi_C2R(cv::Rect(3,3,One.cols, One.rows)));
+
+    // Construct the transition matrix from robot to camera
+    Transi_R2C = Transi_C2R.inv();
 }
 
 cv::Mat Cam::getMatI2C(){
@@ -61,4 +66,21 @@ cv::Mat Cam::getMatR2C(){
     return Transi_R2C;
 }
 
+float Cam::getFocal(){
+    return this->K_C2I.at<int>(0,0);
+}
+Vector2D<int> Cam::getSize(){
+    Vector2D<int> size;
+    size.x = (this->K_C2I.at<int>(0,2)*2) + 1;
+    size.y = (this->K_C2I.at<int>(1,2)*2) + 1;
 
+    return size;
+}
+
+ConfObj3D<float> Cam::getConf(){
+    return conf;
+}
+
+float Cam::getAperAngle(){
+    return aperAngl;
+}
--- primary_robot/linux_image_processing/tools/Cam.h
+++ primary_robot/linux_image_processing/tools/Cam.h
@@ -10,10 +10,10 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <PosObj3D.h>
+#include <ConfObj3D.h>
 #include <Vector2D.h>
 
-template<typename T> class PosObj3D;
+template<typename T> class ConfObj3D;
 template<typename T> class Vector2D;
 
 
@@ -24,14 +24,21 @@ protected:
     cv::Mat K_C2I = cv::Mat(3,3,CV_32F);
     cv::Mat Transi_C2R = cv::Mat(4,4,CV_32F);
     cv::Mat Transi_R2C = cv::Mat(4,4,CV_32F);
+    ConfObj3D<float> conf;
+    float aperAngl;
 
 public:
-    Cam(float f, Vector2D<float> size, PosObj3D<float> posCam);
+    Cam(float f, Vector2D<float> size, ConfObj3D<float> confCam);
 
     cv::Mat getMatI2C();
     cv::Mat getMatC2I();
     cv::Mat getMatC2R();
     cv::Mat getMatR2C();
+    float getFocal();
+    Vector2D<int> getSize();
+    ConfObj3D<float> getConf();
+    float getAperAngle();
+
 };
 
 #endif /* TOOLS_CAM_H_ */
