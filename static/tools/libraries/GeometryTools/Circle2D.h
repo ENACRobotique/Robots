6bdbbb26f3b04fa233212c91f18f69b342fbe2db
Sebastien Malissard <sebastien.malissard@dbmail.com>
2015-04-19 21:12:14 +0200

    [GeometryTools] Adds a const in protecte for Circle2D

--- static/tools/libraries/GeometryTools/Circle2D.h
+++ static/tools/libraries/GeometryTools/Circle2D.h
@@ -69,7 +69,7 @@ class Circle2D {
 
             return p1, p2;
         }
-        Point2D<T> projecte(Point2D<T>& p) const{
+        Point2D<T> projecte(const Point2D<T>& p) const{
             Vector2D<T> v(c, p);
             Point2D<T> pt;
             T n = v.norm();
