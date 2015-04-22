b6d443c0a97287919a4508cc99e93c006384e7b6
YoannSolana <ysolana@laas.fr>
2015-04-22 22:59:19 +0200

    [GeometryTools]In Vector3D.h: Add rotations and operator [].

--- static/tools/libraries/GeometryTools/Vector3D.h
+++ static/tools/libraries/GeometryTools/Vector3D.h
@@ -8,6 +8,9 @@
 #include <opencv2/core/core.hpp>
 #endif
 
+#define RAD2DEG(x) (180.0/(x * M_PI))
+#define DEG2RAD(x) (1/RAD2DEG(x))
+
 template<typename T>
 class Vector3D;
 
@@ -62,6 +65,15 @@ public:
 
     T operator*(const Vector3D& v) const { // Dot product
         return _x * v._x + _y * v._y + _z * v._z;
+    }
+    T operator[](int idx){
+        switch(idx){
+        case 0: return _x; break;
+        case 1: return _y; break;
+        case 2: return _z; break;
+        default: std::cout<<"type Vector3D<T> has no member "<<idx<<std::endl;
+            return -1;  // FIXME: Is it relevant?
+        }
     }
     Vector3D<T> operator^(const Vector3D& v) const { // Cross product
         return Vector3D((_y * v._z - _z * v._y), (-_x * v._z + _z * v._x), (_x * v._y - _y * v._x), _norm && v._norm);
@@ -123,8 +135,61 @@ public:
         _norm = true;
         return *this;
     }
-//    void rotate(const T& theta) {  // TODO
-//    }
+    void rotate( Vector3D<T>& axis, const T& angle, int unit = 0) {  // 0==rad, 1 =deg
+        axis.normalize();
+
+        if(unit)
+            angle = RAD2DEG(angle);
+
+        T A[3][3];
+        for(int row=0; row<3; row++)
+            for(int col=0; col<3; col++)
+                A[row][col] = (1-cos(angle))*axis[row]*axis[col];
+
+        T B[3][3];
+        B[0][0] = B[1][1] = B[2][2] = 0;
+        B[2][1] = sin(angle)*axis[2];
+        B[1][2] = -B[2][1];
+        B[1][3] = sin(angle)*axis[1];
+        B[3][1] = -B[1][3];
+        B[3][2] = sin(angle)*axis[0];
+        B[2][3] = -B[3][2];
+
+        T C[3][3];
+        C[0][1] = C[1][0] = C[0][2] = C[2][0] = C[1][2] = C[2][1] = (T)0;
+        C[0][0] = C[1][1] = C[2][2] = cos(angle);
+
+        T M[3][3];
+        for(int i=0; i<3; i++)
+            for(int j=0; j<3; j++)
+                M[i][j] = A[i][j] + B[i][j] + C[i][j];
+
+        Vector3D temp(this);
+        for(int i=0; i<3; i++)
+            for(int j=0; j<3; j++)
+                this[j] = M[i][j]*temp[j];
+    }
+    void rotate(T Rx, T Ry, T Rz, int unit=0) // 0=rad, 1=deg
+    {
+        if(unit){
+            Rx = DEG2RAD(Rx);
+            Ry = DEG2RAD(Ry);
+            Rz = DEG2RAD(Rz);
+        }
+        T a = cos(Rx);
+        T b = sin(Rx);
+        T c = cos(Ry);
+        T d = sin(Ry);
+        T e = cos(Rz);
+        T f = sin(Rz);
+        T nx = c*e*_x - c*f*_y + d*_z;
+        T ny = (a*f + b*d*e)*_x + (a*e - b*d*f)*_y - b*c*_z;
+        T nz = (b*f - a*d*e)*_x + (a*d*f + b*e)*_y + a*c*_z;
+        this->_x = nx;
+        this->_y = ny;
+        this->_z = nz;
+
+    }
     T angle(const Vector3D& v) {
         return std::acos(*this * v / std::sqrt(normSq() * v.normSq()));
     }
