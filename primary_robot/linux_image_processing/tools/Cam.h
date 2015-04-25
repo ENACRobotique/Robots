cb244ee2ace3730fc34c5087ee0a298041321b14
YoannSolana <ysolana@laas.fr>
2015-04-25 21:45:05 +0200

    [Image_processing]Cam.h: Add get a vector OriginCam->pixel in the  robot frame.

--- primary_robot/linux_image_processing/tools/Cam.h
+++ primary_robot/linux_image_processing/tools/Cam.h
@@ -20,6 +20,7 @@ protected:
     cv::Mat matK_C2I = cv::Mat(3, 3, CV_32F);
     cv::Mat matTransi_C2R = cv::Mat(4, 4, CV_32F);
     cv::Mat matTransi_R2C = cv::Mat(4, 4, CV_32F);
+    cv::Mat matVectOcPx_R = cv::Mat(4,3, CV_32F);
     Transform3D<float> rob2cam;
     cv::Size2f aperAngle;
     cv::Size size;
@@ -49,6 +50,22 @@ public:
 
         // 3D transformation from cam reference frame to robot one
         matTransi_C2R = rob2cam.getReverse().getMatrix();
+
+        // Set a matrix A to stock the constant values to compute the vector OcPx_R (OrigCam2Pixel in the robot)
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(j == 0)
+                    matVectOcPx_R.at<float>(i,j) = matTransi_C2R.at<float>(i,0)*matK_I2C.at<float>(0,0);
+                if(j == 1)
+                    matVectOcPx_R.at<float>(i,j) = matTransi_C2R.at<float>(i,1)*matK_I2C.at<float>(1,1);  // Useful if focal_x = focal_y
+                if(j==2)
+                    matVectOcPx_R.at<float>(i,j) = matTransi_C2R.at<float>(i,3) + matTransi_C2R.at<float>(i,2) +
+                                                   matTransi_C2R.at<float>(i,0)*matK_I2C.at<float>(0,2) +
+                                                   matTransi_C2R.at<float>(i,1)*matK_I2C.at<float>(1,2);
+            }
+        }
+        matVectOcPx_R.at<float>(3,0) = 1;
+        matVectOcPx_R.at<float>(3,1) = matVectOcPx_R.at<float>(3,2) = 0;
     }
 
     const cv::Mat& getMatI2C() const {
@@ -89,6 +106,28 @@ public:
     }
     cv::Mat getBottomLeft() const {
         return (cv::Mat_<float>(2, 1) << 0, size.height - 1);
+    }
+    /*
+     * Description: Compute the vector origin camera to a pixel(u,v) (image cam1) in robot
+     *      OcPx_R = matVectOcPx_R * Px(u,v,1)
+     */
+    cv::Mat getOcPx_R(const cv::Mat px_in) const {
+        cv::Mat px(px_in);
+        if(px_in.cols != 1  &&  px_in.rows < 2){ // Not enough information
+            std::cout<<"getOcPx_R(): (u,v) are missing in the input matrix"<<std::endl;
+            return cv::Mat::zeros(px_in.rows,px_in.cols, CV_8UC1);
+        }
+        else if(px_in.cols != 1  &&  px_in.rows != 4){ // it has u and v but the size is not matched
+            std::cout<<"getOcPx_R(): size of the input matrix changed"<<std::endl;
+            px.create(4,1, CV_32F);
+        }
+        cv::Mat OcPx_R = cv::Mat(4, 1, CV_32F);
+        for(int i=0; i<4; i++){
+            OcPx_R = matVectOcPx_R.at<float>(i,0)*px.at<float>(0) +
+                     matVectOcPx_R.at<float>(i,1)*px.at<float>(1) +
+                     matVectOcPx_R.at<float>(i,2);
+        }
+        return OcPx_R;
     }
 };
 
