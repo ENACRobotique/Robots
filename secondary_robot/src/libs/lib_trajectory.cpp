0f26de6320173e636cdc2eb47acebef8d4a21474
Fabien-B <fabien.bonneval@gmail.com>
2015-05-11 02:38:51 +0200

    [secondary] set heading asservissment to absolute heading (heading at initialisation).

--- secondary_robot/src/libs/lib_trajectory.cpp
+++ secondary_robot/src/libs/lib_trajectory.cpp
@@ -17,22 +17,18 @@
 int _backFromPause = 0;
 
 int periodicProgTrajHeading(trajElem tab[],unsigned long *pausetime, int *i, unsigned long *prev_millis){
-	static int teta0 = headingGetCon();
 	int dt = millis() - *prev_millis -*pausetime;    //time since start of the current traj element
 	int teta = tab[*i].teta*min((dt*FACTOR_HEADING_ASSERV/tab[*i].duration),1);
-	headingSetCon( teta + teta0);
+	headingSetCon( teta);
 
     if (!(*prev_millis)){
     	*prev_millis=millis();
     	speedSetCon(tab[*i].speed);
-        //move(tab[*i].speed,tab[*i].teta);
-        teta0 = headingGetCon();
     }
 
     if(_backFromPause){
     	_backFromPause = 0;
     	speedSetCon(tab[*i].speed);
-    	//move(tab[*i].speed,tab[*i].teta);
 
     }
 
@@ -40,9 +36,7 @@ int periodicProgTrajHeading(trajElem tab[],unsigned long *pausetime, int *i, uns
         (*i)++;
         *prev_millis=millis();
         *pausetime=0;
-        teta0 = headingGetCon();
         speedSetCon(tab[*i].speed);
-        //move(tab[*i].speed,tab[*i].teta);
     }
     if ( tab[*i].teta==0 && tab[*i].duration==0 && tab[*i].speed==0) {
         *i=0;
