8ac8195c5f9df0451d1200ff47d4ba74e591504b
Florian <f.bitard@gmail.com>
2015-05-15 01:32:52 +0200

    [tertiary] Volontary error raised for learner (Flo)

--- tertiary_robot/src/params.h
+++ tertiary_robot/src/params.h
@@ -19,7 +19,7 @@
 //digital I/O
 #define TRIKE
 #define NB_MOTORS 1
-#error "check pin attribution"
+//#error "check pin attribution"
 #define PIN_DIR_SERVO 5
 #define PIN_MOTOR1_PWM 6
 #define PIN_MOTOR1_DIR 7
@@ -27,40 +27,30 @@
 #define PIN_ODO1_SEN 9
 #define PIN_TIRETTE 10
 #define PIN_LED 13
-#define PIN_LAUNCHER_1 8
-#define PIN_LAUNCHER_2 3
-#define PIN_LAUNCHER_NET 4
+#define PIN_CLAP 3
 #define PIN_COLOR 12
-
-//analog in
-#define PIN_SHARP_FRONT_RIGHT A0
-#define PIN_SHARP_FRONT_LEFT A1
-#define PIN_SHARP_BACK_RIGHT A2
-#define PIN_SHARP_BACK_LEFT A3
 
 //times
 #define TIME_MATCH_STOP 90000   //in ms
-#define TIME_FUNNY_STOP 95000
-#define TIME_MATCH_LAUN 85000
 
 //tirette related infos
 #define TIRETTE_IN HIGH
 #define DEBOUNCE_DELAY 100 //(in ms)
 
-#define WALL_DST 15 //in cm<<1
-#define WALL_SPEED 80
-
 #define ANGLE_ZERO      30
 #define DIR_SERVO_START 0// rem : en tenant compte de ANGLE_ZERO
 
-#define COLOR_RED HIGH
-#define COLOR_YELLOW LOW
-
-
-#define UPPERCUT
+#define COLOR_GREEN LOW
+#define COLOR_YELLOW HIGH
 
 #define RADAR_SAFETY_DST 30 //in cm
 #define RADAR_SAFETY_TIME 500 //in ms
+
+#define CLAPNEUTRAL 100
+#define CLAPYELLOW 176
+#define CLAPGREEN 37
+
+#define US_LOWEST_ADDR 0xE2
 
 #define DEBUG
 
@@ -72,7 +62,7 @@
 
 /*  externs*************************************************/
 extern unsigned long _matchStart;
-extern Servo launcherServoUp,launcherServoDown, launcherServoNet;
+extern Servo servoClap;
 
 /*  end externs*********************************************/
 
