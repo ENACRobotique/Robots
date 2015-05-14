3142079ba3858a907711dc8b9688f5e19fa571ab
Florian <f.bitard@gmail.com>
2015-05-15 01:45:10 +0200

    [secondary] Color change for tirette state.

--- secondary_robot/src/states/state_tirette.cpp
+++ secondary_robot/src/states/state_tirette.cpp
@@ -33,7 +33,7 @@ sState* testTirette()
     	{
     		if(timepull==0){timepull = millis();}
 			if(millis() - timepull > TIME_BEFORE_START){
-				if (digitalRead(PIN_COLOR)==COLOR_RED)return &sTrajGreenInit;
+				if (digitalRead(PIN_COLOR)==COLOR_GREEN)return &sTrajGreenInit;
 				else /*return &sAlacon*/return &sTrajYellowInit;
 			}
     	}
