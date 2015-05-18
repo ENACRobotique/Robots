6520cd37051a23a4ae9b3a2834dd9bc810845680
Ludovic Lacoste <ludolacost@gmail.com>
2015-05-19 00:53:18 +0200

    [bn] Moves some projects ; Minor fixes

--- network_config/network_cfg.c
+++ network_config/network_cfg.c
@@ -49,11 +49,11 @@ sRTableEntry rTable[]={
     {SUBNETD2, {IF_I2C,  ADDRI_MAIN_PROP}},
     {0x42&(~SUBNET_MASK),{IF_DROP,0}}
 #elif (MYADDRI == ADDRI_MAIN_PROP || MYADDRU == ADDRU2_MAIN_PROP)
-// project "main_prop_axle/lpc"
+// project "main_prop_axle/lpc" OR project "main_prop_holo/lpc"
     {SUBNETX,  {IF_I2C,  ADDRI_MAIN_TURRET}},
     {SUBNETU1, {IF_I2C,  ADDRI_MAIN_IO}},
     {SUBNETD1, {IF_I2C,  ADDRI_MAIN_IO}},
-    {SUBNETD2, {IF_UART, ADDRU2_MAIN_AI}},
+    {SUBNETD2, {IF_UART, ADDRU2_MAIN_AIBRIDGE}},
     {0x42&(~SUBNET_MASK),{IF_DROP,0}}
 #elif (MYADDRX == ADDRX_MOBILE_1 || MYADDRX == ADDRX_MOBILE_2 || MYADDRX == ADDRX_DEBUG || MYADDRX == ADDRX_FIX)
 // project "balise_mobile" OR debug node
@@ -63,23 +63,31 @@ sRTableEntry rTable[]={
     {SUBNETD1, {IF_XBEE, ADDRX_MAIN_TURRET}},
     {SUBNETD2, {IF_XBEE, ADDRX_MAIN_TURRET}},
     {0x42&(~SUBNET_MASK),{IF_DROP,0}}
-#elif (MYADDRD == ADDRD1_DEBUG1 || MYADDRD == ADDRD1_DEBUG2  || MYADDRD == ADDRD1_DEBUG3 || MYADDRD == ADDRD1_MAIN_PROP_SIMU || MYADDRD == ADDRD1_MAIN_AI_SIMU || MYADDRD == ADDRD1_MONITORING)
-// project "bn_debug_console" OR project "main_prop_axle/linux" OR project "main_ai" OR project "monitoring_hmi/*"
+#elif (MYADDRD == ADDRD1_DEBUG1 || MYADDRD == ADDRD1_DEBUG2  || MYADDRD == ADDRD1_DEBUG3 || MYADDRD == ADDRD1_MAIN_PROP_SIMU || MYADDRD == ADDRD1_MAIN_AI_SIMU || MYADDRD == ADDRD1_MONITORING || MYADDRD == ADDRD1_MAIN_VIDEO_SIMU)
+// project "bn_debug_console" OR project "main_prop_axle/linux" OR project "main_prop_holo/linux" OR project "main_ai/dbg" OR project "monitoring_hmi/*" OR project "linux_image_processing/dbg"
     {SUBNETX,  {IF_UDP,  ADDRD1_DBGBRIDGE}},
     {SUBNETI,  {IF_UDP,  ADDRD1_DBGBRIDGE}},
     {SUBNETU1, {IF_UDP,  ADDRD1_DBGBRIDGE}},
     {SUBNETU2, {IF_UDP,  ADDRD1_DBGBRIDGE}},
     {SUBNETD2, {IF_UDP,  ADDRD1_DBGBRIDGE}},
     {0x42&(~SUBNET_MASK),{IF_DROP,0}}
-#elif (MYADDRU == ADDRU2_MAIN_AI || MYADDRD == ADDRD2_MAIN_AI)
-// project "main_ai"
+#elif (MYADDRD == ADDRD2_MAIN_AI || MYADDRD == ADDRD2_MAIN_VIDEO)
+// project "main_ai/bbb" OR project "linux_image_processing/bbb"
+    {SUBNETX,  {IF_UDP,  ADDRD2_MAIN_AIBRIDGE}},
+    {SUBNETI,  {IF_UDP,  ADDRD2_MAIN_AIBRIDGE}},
+    {SUBNETU1, {IF_UDP,  ADDRD2_MAIN_AIBRIDGE}},
+    {SUBNETU2, {IF_UDP,  ADDRD2_MAIN_AIBRIDGE}},
+    {SUBNETD1, {IF_UDP,  ADDRD2_MAIN_AIBRIDGE}},
+    {0x42&(~SUBNET_MASK),{IF_DROP,0}}
+#elif (MYADDRU == ADDRU2_MAIN_AIBRIDGE || MYADDRD == ADDRD2_MAIN_AIBRIDGE)
+// project "bn_bridge/ai"
     {SUBNETX,  {IF_UART, ADDRU2_MAIN_PROP}},
     {SUBNETI,  {IF_UART, ADDRU2_MAIN_PROP}},
     {SUBNETU1, {IF_UART, ADDRU2_MAIN_PROP}},
     {SUBNETD1, {IF_UART, ADDRU2_MAIN_PROP}},
     {0x42&(~SUBNET_MASK),{IF_DROP,0}}
 #elif (MYADDRU == ADDRU1_DBGBRIDGE || MYADDRD == ADDRD1_DBGBRIDGE)
-// project "debug_bridge"
+// project "bn_bridge/dbg"
     {SUBNETX,  {IF_UART, ADDRU1_MAIN_IO}},
     {SUBNETI,  {IF_UART, ADDRU1_MAIN_IO}},
     {SUBNETU2, {IF_UART, ADDRU1_MAIN_IO}},
