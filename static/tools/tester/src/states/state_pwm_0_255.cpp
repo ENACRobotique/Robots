268e7e7dbf16df177ed65e7b18a199f1222a3a17
Fabien-B <fabien.bonneval@gmail.com>
2015-06-18 18:34:42 +0200

    [tester] do 0->255 pwm.

--- static/tools/tester/src/states/state_pwm_0_255.cpp
+++ static/tools/tester/src/states/state_pwm_0_255.cpp
@@ -14,70 +14,54 @@
 #include "state_Menu_pwm.h"
 #include "state_pwm_0_255.h"
 #include "lib_IHM.h"
+#include "state_choice_servo.h"
 
 
 sState* testpwm_0_255(){
-	static int memValue=0;
 	static long temps_enc=0;
 	static int pos_enc_old=myEnc.read();
 
-	int pos_enc=myEnc.read()/2;
-	if (pos_enc < 0)
-	{
-		pos_enc=0;
-		myEnc.write(0);
-	}
-	int delta=1;
+	int pos_enc = myEnc.read();
 
-	if(pos_enc!=pos_enc_old)
-	{
-		long deltat=millis()-temps_enc;
-		if(deltat<DUREE_BIG_STEPS)
+	if(pos_enc!=pos_enc_old){
+		if(millis()-temps_enc < DUREE_BIG_STEPS)
 		{
-			delta=10;
+			pos_enc = pos_enc_old + 10 * (pos_enc - pos_enc_old);
 		}
+		pos_enc = CLAMP(0, pos_enc, 255);
 		temps_enc=millis();
-	}
-	int Value = max((memValue+delta*(pos_enc-pos_enc_old)),0);
-	Value=min(Value,255);
-	pos_enc_old=pos_enc;
 
-	if(!digitalRead(SELECT))	//nécessite de valider avant que le servo ne se déplace
-	{
-		analogWrite(PIN_PWM_SERVO,Value);
+		afficher("D.C.= %d /255", pos_enc);
+		myEnc.write(pos_enc);
+		pos_enc_old=pos_enc;
 	}
 
-	if(Value!=memValue)
-	{
-		char affich[16];
-		snprintf(affich,17,"Pos= %d /255",Value);
-		afficher(affich);
-		memValue=Value;
+	if(!digitalRead(SELECT)){	//nécessite de valider avant que le servo ne se déplace
+		while(!digitalRead(SELECT));
+		analogWrite(PIN_PWM_SERVO,pos_enc);
 	}
 
-	if(!digitalRead(RETOUR))
-	{
+	if(!digitalRead(RETOUR)){
 		delay(DELAY_BOUNCE);	//anti rebond
 		while(!digitalRead(RETOUR));	//attente du relachement du bouton
 		return(&sMenu_pwm);
 	}
     return NULL;
 }
+
 void initpwm_0_255(sState *prev){
+	if(servo3.attached()){
+		servo3.detach();
+	}
 	pinMode(PIN_PWM_SERVO,OUTPUT);
 	myEnc.write(0);
 	analogWrite(PIN_PWM_SERVO,0);
-	char affich[16];
-	snprintf(affich,17,"Pos= 0 /255");
-	afficher(affich);
+	afficher("Pos= 0 /255");
 }
+
 void deinitpwm_0_255(sState *next){
-
 }
 
-void pwm_0_255(){
-
-}
 
 sState spwm_0_255={
     0,
