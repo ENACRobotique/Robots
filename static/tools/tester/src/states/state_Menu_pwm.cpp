5d9f2b6f68a2dafe66b4b2592f5726afb17607ac
Fabien-B <fabien.bonneval@gmail.com>
2015-06-18 18:34:03 +0200

    [tester] do pwm menu same ways than the servo one.

--- static/tools/tester/src/states/state_Menu_pwm.cpp
+++ static/tools/tester/src/states/state_Menu_pwm.cpp
@@ -6,8 +6,8 @@
  */
 
 #include "Arduino.h"
-#include "../params.h"
-#include "../tools.h"
+#include "params.h"
+#include "tools.h"
 #include "state_types.h"
 
 #include "state_Menu_principal.h"
@@ -18,52 +18,51 @@
 #include "state_pwm_0_5.h"
 #define NB_menu_pwm 3
 
-sState* testMenu_pwm(){
-	const char *menu_pwm[] = {
+const char *menu_pwm[] = {
 		  "  0 - 255",
-		  "   %",
-		  "  0 - 5 V",
+		  " 0 - 100 %",
+		  "  0 - 15 V",
 		};
 
+sState* testMenu_pwm(){
+	static int memPosition;
+	int Position = myEnc.read();    //position du selecteur modulo le nombre de choix possible
 
-		static int memPosition;
-		int Position = (myEnc.read()/2)%NB_menu_pwm;    //position du selecteur
-		   if(Position != memPosition)  //on affiche que si on change de position
-		   {
-		      afficher(menu_pwm[Position]);
-		      memPosition=Position;
-		   }
+	if(Position != memPosition){  //on affiche que si on change de position
+		if (Position != CLAMP(0,Position,NB_menu_pwm)){		//on ne descend pas dans les négatifs
+			Position = CLAMP(0,Position,NB_menu_pwm);
+			myEnc.write(Position);
+		}
+		afficher(menu_pwm[Position]);
+	    memPosition=Position;
+	}
 
-		  if(!digitalRead(SELECT))
-		  {
-			while(!digitalRead(SELECT));
-		    switch (Position)
-		    {
-		        case 0:{ return(&spwm_0_255); break; }
-		        case 1:{ return(&spwm_pc); break; }
-		        case 2:{ return(&spwm_0_5); break; }
-		        //default:
-		     }
+	if(!digitalRead(SELECT)){	//si on appui sur select:
+		while(!digitalRead(SELECT));		//on attend qu'on relache
+
+		switch (Position)		//et on va dans le bon état
+		{
+			case 0:{ return(&spwm_0_255); break; }
+			case 1:{ return(&spwm_pc); break; }
+			case 2:{ return(&spwm_0_5); break; }
 		  }
+	  }
 
-		  if(!digitalRead(RETOUR))
-		  {
-			  delay(DELAY_BOUNCE);	//anti rebond
-			  while(!digitalRead(RETOUR));	//attente du relachement du bouton
-			  return(&sMenu_principal);
-		  }
+	if(!digitalRead(RETOUR)){
+		delay(DELAY_BOUNCE);	//anti rebond
+		while(!digitalRead(RETOUR));	//attente du relachement du bouton
+		return(&sMenu_principal);
+	}
 
-
-    return NULL;
+return NULL;
 }
+
 void initMenu_pwm(sState *prev){
 			myEnc.write(0);
-			afficher("  0 - 255");
+			afficher(menu_pwm[0]);
 }
 void deinitMenu_pwm(sState *next){
-
 }
-
 
 sState sMenu_pwm={
     0,
