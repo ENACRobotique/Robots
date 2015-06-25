99d0c446134ad87f0187e4dafed8f5fefb745b6a
Fabien-B <fabien.bonneval@gmail.com>
2015-06-25 14:21:31 +0200

    [tester] replace median filter by a low pass filter for the coder.

--- static/tools/tester/src/states/state_Codeur.cpp
+++ static/tools/tester/src/states/state_Codeur.cpp
@@ -15,27 +15,27 @@
 #include "lib_IHM.h"
 #include "lib_odo.h"
 #include "state_Codeur_nb_inc_choice.h"
-extern "C" {
-#include "median_filter.h"
-}
+
+#define FILTER_SHIFT 2
 
 #define NB_VISU 4
 int inc_t = 3200;
-median_t mf;
 
 sState* testCodeur(){
+	static int nbInc_filtered=0;
 	static unsigned long prev_time = millis();
-	if(millis()-prev_time > 499){
-		//////////////////////////////// lecture de l'encodeur pour choisir le mode de visualisation
-		int Position = myEnc.read();
-		////////////////////////////////
-		mf_update(&mf, odoRead());
-		int nbInc = mf_get(&mf);
-		//rajouter un median filter pour lisser les valeurs!
+	if(millis()-prev_time > 249){
+		int Position = myEnc.read(); //lecture de l'encodeur pour choisir le mode de visualisation
+
+		//filtre passe bas : nb_filtered = a*nb_now + (1-a)*last_nb		avec a entre 0 et 1.
+		int nbInc_now = odoRead();
+		nbInc_filtered = nbInc_filtered - (nbInc_filtered >> FILTER_SHIFT) + nbInc_now;
+		nbInc_now = nbInc_filtered >> FILTER_SHIFT;
+
 		switch (Position)		//et on va dans le bon état
 		{
 			case 0:{		//en tours par secondes
-				int N_ts = (400.0*nbInc)/inc_t;			//400 = 100 * (1000/500) * 2(int sur voie A seulement)
+				int N_ts = (800.0*nbInc_now)/inc_t;			//800 = 100 * (1000/250) * 2(int sur voie A seulement)
 				int ent = abs(N_ts / 100);
 				int dec = abs(N_ts - 100*ent);
 				char sign = ' ';
@@ -43,17 +43,17 @@ sState* testCodeur(){
 				afficher("N(t/s)= %c%d,%d",sign,ent,dec);
 				break; }
 			case 1:{		//en rd/s
-				int W_rs = (400.0*nbInc*TWO_PI)/inc_t;	//400 = 100 * (1000/500) * 2(int sur voie A seulement)
+				int W_rs = (800.0*nbInc_now*TWO_PI)/inc_t;	//800 = 100 * (1000/250) * 2(int sur voie A seulement)
 				int ent = W_rs / 100;
 				int dec = abs(W_rs - 100*ent);
 				afficher("W(r/s)= %d,%d",ent,dec);
 				break; }
 			case 2:{
-				int N_ds = (1440.0*nbInc)/inc_t;		//1440 = 360° * (1000/500) * 2(int sur voie A seulement)
+				int N_ds = (2880.0*nbInc_now)/inc_t;		//2880 = 360° * (1000/250) * 2(int sur voie A seulement)
 				afficher("N(d/s)= %d",N_ds);
 				break; }
 			case 3:{
-				int N_is = nbInc*4;						//4 = (1000/500) * 2(int sur voie A seulement)
+				int N_is = nbInc_now*8;						//8 = (1000/250) * 2(int sur voie A seulement)
 				afficher("nb_inc/s= %d",N_is);
 				break; }
 		}
@@ -79,7 +79,7 @@ void initCodeur(sState *prev){
 	myEnc.setLimits(0,NB_VISU-1);
 	myEnc.write(0);
 	myEnc.setMultiplicators(1,1);
-	mf_init(&mf, 15, 0);
+//	mf_init(&mf, 15, 0);
 }
 
 void deinitCodeur(sState *next){
