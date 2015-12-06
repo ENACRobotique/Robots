62e5d951e298d5ff7365b4e0717b25ed7d4a9b5b
Darian <darian.motamed@gmail.com>
2015-12-06 20:44:23 +0100

    [secondary] Change pin configuration.

--- secondary_robot/src/params.h
+++ secondary_robot/src/params.h
@@ -13,31 +13,32 @@
 
 /* endincludes *********************************************/
 
-#define HEADING
-#define ATTITUDE
+//#define HEADING
+//#define ATTITUDE
 
 //#define NO_ATTITUDE_BEFORE_STAIRS
 
-#define TIME_BEFORE_START 1000
+#define TIME_BEFORE_START 10
 /* Defines ************************************************/
 //pins
 //digital I/O
-#define TANK
-#define NB_MOTORS 2
-#define PIN_MOTOR1_PWM 5
-#define PIN_MOTOR1_DIR 12
+#define TRIKE
+#define PIN_SERVO_DIR 5
+#define NB_MOTORS 1
+#define PIN_MOTOR1_PWM 6
+#define PIN_MOTOR1_DIR 7
 #define PIN_ODO1_INT 2//odometer1, interruption pin
-#define PIN_ODO1_SEN 7
-#define PIN_MOTOR2_PWM 6
-#define PIN_MOTOR2_DIR 13
-#define PIN_ODO2_INT 3//odometer2, interruption pin
-#define PIN_ODO2_SEN 8
-#define PIN_TIRETTE A0
+#define PIN_ODO1_SEN 9
+//#define PIN_MOTOR2_PWM 6
+//#define PIN_MOTOR2_DIR 13
+//#define PIN_ODO2_INT 3//odometer2, interruption pin
+//#define PIN_ODO2_SEN 8
+#define PIN_TIRETTE 10
 //#define PIN_LED 13
-#define PIN_COLOR 4
-#define PIN_SERVO_ATTITUDE 10
-#define PIN_SERVO_CARPET 9
-#define PIN_VENTILO 11
+#define PIN_COLOR 12
+//#define PIN_SERVO_ATTITUDE 10
+//#define PIN_SERVO_CARPET 9
+//#define PIN_VENTILO 11
 //analog in
 //#define PIN_SHARP_FRONT_RIGHT A0
 //#define PIN_SHARP_FRONT_LEFT A1
@@ -45,7 +46,9 @@
 //#define PIN_SHARP_BACK_LEFT A3
 
 //times
-#define TIME_MATCH_STOP 88000   //in ms  en théorie 90s mais on prend un peu de marge
+#define TIME_MATCH_STOP 90000   //in ms  en théorie 90s mais on prend un peu de marge
+
+#define ANGLE_ZERO 108
 
 //tirette related infos
 #define TIRETTE_IN HIGH
@@ -57,14 +60,12 @@
 #define COLOR_RED HIGH
 #define COLOR_YELLOW LOW
 
-#define UPPERCUT
 
 #define RADAR_SAFETY_DST 30 //in cm
 #define RADAR_SAFETY_TIME 500 //in ms
 
 #define TIME_READY_LAUNCHER 500 //in ms
 
-#define FAN_SPEED 254 //PWM Command(0-255)
 
 #define DEBUG 1
 
