1dd8e1bf13f5efd812909c5c9af35f719b546e4e
Darian <darian.motamed@gmail.com>
2016-02-05 21:03:13 +0100

    [secondary] Add new state 'Recalage'.

---
+++ secondary_robot/src/states/state_Recalage.cpp
@@ -0,0 +1,62 @@
+/*
+ * state_Recalage.cpp
+ *
+ *  Created on: 2016 février 05
+ *      Author: Darian
+ */
+
+
+#include "Arduino.h"
+#include "state_Recalage.h"
+#include "state_types.h"
+#include "lib_move.h"
+#include "lib_radar.h"
+#include "lib_line.h"
+#include "lib_wall.h"
+#include "lib_attitude.h"
+#include "lib_heading.h"
+#include "lib_trajectory.h"
+#include "../tools.h"
+#include "../params.h"
+#include "state_traj.h"
+#include "state_pause.h"
+#include "state_wait.h"
+#include "state_lineMonit.h"
+#include "lib_radar_mask.h"
+
+sState* testRecalage(){
+        trajElem calage_longueur[] = {
+				{800,0,500},
+				{0,-90,300},
+				{650,-90,1300},
+				{0,0,100},
+				{-800,0,6000},
+				{0,0,0}
+        };
+        static unsigned long st_saveTime=0;
+        static int i=0;
+		static int i_radar=0;
+		static unsigned long prev_millis=0;
+		static unsigned long prev_millis_radar=0;
+		if(periodicProgTraj(calage_longueur,&st_saveTime,&i,&prev_millis))
+		{
+		    return NULL;
+		}
+
+	}
+
+void initRecalage(sState *prev){
+        // Your code here !
+    }
+
+void deinitRecalage(sState *next){
+        // Your code here !
+    }
+
+sState sRecalage={
+	BIT(E_MOTOR),
+    &initRecalage,
+    &deinitRecalage,
+    &testRecalage
+};
+
---
+++ secondary_robot/src/states/state_Recalage.h
@@ -0,0 +1,15 @@
+/*
+ * state_Recalage.h
+ *
+ *  Created on: 2016 février 05
+ *      Author: Darian
+ */
+
+#ifndef STATE_RECALAGE_H_
+#define STATE_RECALAGE_H_
+
+#include "state_types.h"
+
+extern sState sRecalage;
+
+#endif /* STATE_RECALAGE_ */
