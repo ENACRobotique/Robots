dbd1cf86df8de4bff3cba92876581c27d14711da
Florian Liehn <florian@liehn.com>
2016-02-21 22:00:27 +0100

    [secondary] Upate TIME_MATCH and TIME_FOR_FUNNY_ACTION .

--- secondary_robot/src/params.h
+++ secondary_robot/src/params.h
@@ -41,8 +41,8 @@
 
 
 //times
-#define TIME_MATCH_STOP 10000   //in ms  en théorie 90s mais on prend un peu de marge
-#define TIME_FOR_FUNNY_ACTION 5000 // the beginning of the funny action
+#define TIME_MATCH_STOP 94000   //in ms  en théorie 90s mais on prend un peu de marge
+#define TIME_FOR_FUNNY_ACTION 90000 // the beginning of the funny action
 
 #define ANGLE_ZERO 108
 
