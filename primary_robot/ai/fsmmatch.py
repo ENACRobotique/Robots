582422cef51e11ef3401d5b1f90dc0d1535deeb9
EngelOfChipolata <engelofdeath@live.fr>
2017-04-27 19:18:06 +0200

    [AI] FSMMatch inherits from Behavior

--- primary_robot/ai/fsmmatch.py
+++ primary_robot/ai/fsmmatch.py
@@ -1,17 +1,20 @@
-class FSMMatch:
+from behavior import Behavior
+
+
+class FSMMatch(Behavior):
     def __init__(self, robot):
-        self.state = StateInit()
+        self.state = StateInit(robot)
         self.robot = robot
 
     def loop(self):
         next_state = self.state.test()
         if next_state is not None:
             self.state.deinit()
-            self.state = next_state()
+            self.state = next_state(self.robot)
 
 
 class FSMState:
-    def __init__(self):
+    def __init__(self, robot):
         raise NotImplementedError("this state is not defined yet")
 
     def test(self):
@@ -22,4 +25,11 @@ class FSMState:
 
 
 class StateInit(FSMState):
-    pass
+    def __init__(self, robot):
+        pass
+
+    def test(self):
+        pass
+
+    def deinit(self):
+        pass
