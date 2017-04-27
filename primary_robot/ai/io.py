9323a9b9d9070f0d9dc3cc8a2466dc6c2550cb50
EngelOfChipolata <engelofdeath@live.fr>
2017-04-27 22:02:30 +0200

    [AI] Completes IO module

--- primary_robot/ai/io.py
+++ primary_robot/ai/io.py
@@ -26,28 +26,68 @@ class IO:
         OPEN = "open"
         CLOSED = "close"
 
+    class RocketLauncherState(Enum):
+        LOCKED = "locked"
+        OPEN = "open"
+
     def __init__(self, robot):
         self.robot = robot
         self.cord_state = None
         self.color = None
         self.ball_picker_state = None
         self.cannon_state = None
+        self.cannon_barrier_state = None
+        self.rocket_launcher_state = None
         self.stop_ball_picker()
         self.stop_cannon()
+        self.close_cannon_barrier()
+        self.lock_rocket_launcher()
 
     def start_ball_picker(self):
-        self.robot.communication.send_start_motor(BALL_PICKER_MOTOR)  #TODO : faire cette fonction
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.START_BALL_PICKER_MOTOR
+        self.robot.communication.send_message(down_msg)
         self.ball_picker_state = self.BallPickerState.STARTED
         pass
 
     def stop_ball_picker(self):
-        self.robot.communication.send_stop_motor(BALL_PICKER_MOTOR)  #TODO: faire cette fonction
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.STOP_BALL_PICKER_MOTOR
+        self.robot.communication.send_message(down_msg)
         self.ball_picker_state = self.BallPickerState.STOPPED
 
     def start_cannon(self):
-        self.robot.communication.send_start_motor(CANNON_MOTOR)  #TODO: faire cette fonction
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.START_CANNON_MOTOR
+        self.robot.communication.send_message(down_msg)
         self.cannon_state = self.CannonState.FIRING
 
     def stop_cannon(self):
-        self.robot.communication.send_stop_motor(CANNON_MOTOR)  # TODO: faire cette fonction
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.STOP_CANNON_MOTOR
+        self.robot.communication.send_message(down_msg)
         self.cannon_state = self.CannonState.IDLE
+
+    def close_cannon_barrier(self):
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.CLOSE_CANNON_BARRIER
+        self.robot.communication.send_message(down_msg)
+        self.cannon_barrier_state = self.CannonBarrierState.CLOSED
+
+    def open_cannon_barrier(self):
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.OPEN_CANNON_BARRIER
+        self.robot.communication.send_message(down_msg)
+        self.cannon_barrier_state = self.CannonBarrierState.OPEN
+
+    def lock_rocket_launcher(self):
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.LOCK_ROCKET_LAUNCHER
+        self.robot.communication.send_message(down_msg)
+        self.rocket_launcher_state = self.RocketLauncherState.LOCKED
+
+    def open_rocket_launcher(self):
+        down_msg = self.robot.communication.sMessageDown()
+        down_msg.message_type = self.robot.communication.eTypeDown.OPEN_ROCKET_LAUNCHER
+        self.robot.communication.send_message(down_msg)
+        self.rocket_launcher_state = self.RocketLauncherState.OPEN
