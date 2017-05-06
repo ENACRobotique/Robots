d4f31be86b3f8571445cb41c021ebc6dd8aa98e4
Fabien-B <fabien.bonneval@gmail.com>
2017-05-06 05:07:33 +0200

    [Primary] Set pin numbers for the PCB board.

--- primary_robot/propulsion/src/params.h
+++ primary_robot/propulsion/src/params.h
@@ -8,16 +8,30 @@
 #ifndef PARAMS_H_
 #define PARAMS_H_
 
-
-#define PWM_LEFT 6
-#define DIR_LEFT 7
-#define PWM_RIGHT 8
-#define DIR_RIGHT 9
-
 #define ODO_I_LEFT 2
 #define ODO_S_LEFT 3
 #define ODO_I_RIGHT 4
 #define ODO_S_RIGHT 5
+
+#define DIR_LEFT 6
+#define PWM_LEFT 7
+#define DIR_RIGHT 8
+#define PWM_RIGHT 9
+
+#define SERVO1 20
+#define SERVO2 21
+#define SERVO3 22
+#define SERVO4 23
+
+#define TIRETTE 27
+#define COLOR 28
+#define MOTOR_PICKUP 29
+#define MOTOR_LAUNCHER 30
+
+#define IHM_BONUS 35
+#define LED_RED 36
+#define LED_GREEN 37
+#define LED_BLUE 38
 
 #define UPDATE_PERIOD 0.02f
 
