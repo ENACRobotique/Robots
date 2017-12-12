[kalman] Correct innovation computation and add yaw plot

static/localization/kalman/acquire_data.py
@@ -10,6 +10,7 @@ from enum import Enum
 from command import Command
 from measurement import SimpleMeasurement
 from sensor import BasicSensor
+from state import State
 import numpy as np
 from random import normalvariate
 
@@ -17,12 +18,19 @@ BLENDER_X_RANGE = 3
 BLENDER_Y_RANGE = 2
 REAL_X_RANGE = 3
 REAL_Y_RANGE = 2
-MORSE_MEASUREMENT_X_STD = MORSE_MEASUREMENT_Y_STD = 0.02  # in m
-MORSE_MEASUREMENT_YAW_STD = 2 * np.pi / 180  # in rad
+
+MORSE_MEASUREMENT_X_STD = MORSE_MEASUREMENT_Y_STD = 0.05  # in m
+MORSE_MEASUREMENT_YAW_STD = 3 * np.pi / 180  # in rad
+
+MORSE_ODO_TOTAL_SPEED_STD = 0.3  # in m/s
+MORSE_ODO_ROT_SPEED_STD = 15 * np.pi / 180  # in rad/sec
+
+
 
 class DATA_TYPE(Enum):
     COMMAND = 0
     MEASUREMENT = 1
+    ACTUAL = 2
 
 
 def get_stimulated_odometry(queue, lock):
@@ -54,12 +62,18 @@ def get_morse_odometry(queue, lock):
             # Create command
             command = Command(total_speed, rotation_speed, current_time)
 
+            # Add noise if moving
+            if command.has_noise():
+                command.total_speed += normalvariate(0, MORSE_ODO_TOTAL_SPEED_STD)
+                command.rotation_speed += normalvariate(0, MORSE_ODO_ROT_SPEED_STD)
+
             # Write in queue to process
             lock.acquire()
             queue.put((DATA_TYPE.COMMAND, command))
             lock.release()
 
-            time.sleep(0.02)
+            time.sleep(0.05)
+
 
 def get_morse_measurement(queue, lock):
     morse_sensor = BasicSensor(np.diag([MORSE_MEASUREMENT_X_STD, MORSE_MEASUREMENT_Y_STD, MORSE_MEASUREMENT_YAW_STD]))
@@ -74,16 +88,24 @@ def get_morse_measurement(queue, lock):
             # Shift into table coordinates
             (x, y) = blender2real(x_blender, y_blender)
 
+            # Saved true state
+            true_state = State(x, y, yaw)
+
+            # Write actual state in queue to process
+            lock.acquire()
+            queue.put((DATA_TYPE.ACTUAL, true_state))
+            lock.release()
+
             # Add Gaussian noise
             x_measured = normalvariate(x, MORSE_MEASUREMENT_X_STD)
             y_measured = normalvariate(y, MORSE_MEASUREMENT_Y_STD)
             yaw_measured = normalvariate(yaw, MORSE_MEASUREMENT_YAW_STD) #% (2 * np.pi)
             measurement = SimpleMeasurement(x_measured, y_measured, yaw_measured, morse_sensor)
 
-            # Write in queue to process
+            # Write measurement in queue to process
             lock.acquire()
             queue.put((DATA_TYPE.MEASUREMENT, measurement))
             lock.release()
 
-            time.sleep(0.1)
+            time.sleep(0.2)
 

static/localization/kalman/main.py
@@ -12,20 +12,21 @@ import copy
 import matplotlib.pyplot as plt
 
 PLOT_MODE = True  # Set to false to disable plotting
-PLOT_RATE = 3  # Frequency of plot (Hz)
+PLOT_RATE = 4  # Frequency of plot (Hz)
 
 # Data receiving threads
 data_queue = queue.Queue() # Contains tuple of the form (DATA_TYPE, data)
 data_queue_lock = Lock()
-# thread_odo = Thread(target=acquire_data.get_stimulated_odometry, args=(data_queue, data_queue_lock,))
+
 thread_odo = Thread(target=acquire_data.get_morse_odometry, args=(data_queue, data_queue_lock,))
 thread_odo.start()
+
 thread_measurement1 = Thread(target=acquire_data.get_morse_measurement, args=(data_queue, data_queue_lock,))
 thread_measurement1.start()
 
 
 # Create motion model
-Q = np.diag([0.005, 4 * np.pi / 180])  # Represent model uncertainties
+Q = np.diag([0.01, 3 * np.pi / 180])  # Represent model uncertainties
 odometry_model = model.OdometricModel(Q)
 
 # Initialize kalman filter
@@ -36,6 +37,7 @@ current_state = initial_state
 current_error = initial_error_matrix
 previous_print_state = None
 previous_print_error = None
+true_state = None
 
 # Plot
 if PLOT_MODE:
@@ -48,7 +50,7 @@ synchronized = False
 
 while True:
     if data_queue.qsize() > 0:
-        print("Q size :" + str(data_queue.qsize()))
+        # print("Q size :" + str(data_queue.qsize()))
         # Get the older data (command or measurement)
         data_queue_lock.acquire()
         data_type, data = data_queue.get()
@@ -59,8 +61,8 @@ while True:
             if synchronized:  # Need to synchronize clock before computing any prediction
                 kf.predict(data)
 
-                current_state = copy.deepcopy(kf.state)
-                current_error = copy.deepcopy(kf.error_matrix)
+                # current_state = copy.deepcopy(kf.state)
+                # current_error = copy.deepcopy(kf.error_matrix)
 
             else:
                 synchronized = True
@@ -71,16 +73,34 @@ while True:
         # Update
         elif data_type == acquire_data.DATA_TYPE.MEASUREMENT:
             kf.update(data)
-            current_state = copy.deepcopy(kf.state)
-            current_error = copy.deepcopy(kf.error_matrix)
+
+        # Save actual state (if given by the simulation)
+        elif data_type == acquire_data.DATA_TYPE.ACTUAL:
+            true_state = data
+            continue
+
+        current_state = copy.deepcopy(kf.state)
+        current_error = copy.deepcopy(kf.error_matrix)
 
         # Plot
         if PLOT_MODE:
             # if plot.need_to_refresh(current_state, previous_print_state) and time.time() - last_plot_time > 1 / PLOT_RATE:
             if time.time() - last_plot_time > 1 / PLOT_RATE:
 
+                ax.clear()
+                ax.set_xlim(0, 3)
+                ax.set_ylim(0, 2)
 
+                # Plot error ellipse
                 plot.plot_error_matrix(copy.deepcopy(kf.error_matrix), copy.deepcopy(kf.state), ax)
+
+                # Plot estimated state
+                plot.plot_estimated_state(current_state, ax)
+
+                # Plot last actual state if exists
+                if true_state is not None:
+                    plot.plot_actual_state(true_state, ax)
+
                 plt.draw()
 
                 last_plot_time = time.time()

static/localization/kalman/sensor.py
@@ -23,7 +23,7 @@ class BasicSensor():
         return np.eye(len(state))
 
     def innovation(self, expected_measurement, measurement):
-        angle_difference = (measurement.theta - expected_measurement.theta) % 2 * np.pi
+        angle_difference = (measurement.theta - expected_measurement.theta) % (2 * np.pi)
         if angle_difference >= np.pi:
             angle_difference -= 2 * np.pi
         innovation = np.array([measurement.x - expected_measurement.x, measurement.y - expected_measurement.y,