6d0aad176b6c77332aada81100870389f878c2c1
Darian <darian.motamed@gmail.com>
2018-05-10 13:19:50 +0200

    Add Domotic code

---
+++ static/tools/DomoPanel/CountPanel/includer.h
@@ -0,0 +1,5 @@
+//Just include the correct lib
+
+//USE YOUR ABSOLUTPATH
+//#include "../../libraries/RGBmatrixDriver/Adafruit_GFX_Library/Adafruit_GFX.h"   // Core graphics library
+//#include "../../libraries/RGBmatrixDriver/RGBmatrixPanel.h" // Hardware-specific library
