13b9b1c00acca143cc8f150880c0f6673b9239f9
Florian Liehn <florian@liehn.com>
2018-09-16 01:48:15 +0200

    [DomoPanel] Add color feedback

--- static/tools/DomoPanel/CustomPanel/IHM/MatrixGui.py
+++ static/tools/DomoPanel/CustomPanel/IHM/MatrixGui.py
@@ -1,15 +1,18 @@
 #! /usr/bin/python
 from tkinter import *
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 from tkinter.colorchooser import askcolor
 import Image
+import numpy as np
 
 LED_PIXELS_SIZE=15
-
+CURRENT_COLOR_TEXT="Crayon:({:03},{:03},{:03})"
+CURRENT_PIXEL_TEXT="Pixel:({:03},{:03},{:03})"
 
 class MatrixGui:
 	def __init__(self, master):
 
-		self.matrix=[[(0,0,0)for y in range(16)]for x in range(32)]
+		self.matrix=[[(0,0,0) for y in range(16)]for x in range(32)]
 		self.currentColor=(0,0,0)
 
 		self.master = master
@@ -30,63 +33,97 @@ class MatrixGui:
 		
 		self.drawMatrix()
 		self.canvasMatrix.bind("<B1-Motion>", self.penCallback)
+		self.canvasMatrix.bind("<Motion>", self.moveMouseOverPixel)
 		self.canvasMatrix.pack()
 
 		#Pen Information
 		self.PenOptionFrame=Frame(master)
 		self.PenOptionFrame.pack()
 
-		self.colorButton=Button(self.PenOptionFrame,text='Select Color', command=self.getColor)
+		self.colorButton=Button(self.PenOptionFrame,text='Select Color', command=self.setColor)
 		self.colorButton.pack(side=LEFT,padx=50)
 
-		self.currentPenColorLabel=Label(self.PenOptionFrame, text="Couleur crayon!")
+		self.currentPenColorVar=StringVar()
+		self.currentPenColorVar.set(CURRENT_COLOR_TEXT.format(0,0,0) )
+		self.currentPenColorLabel=Label(self.PenOptionFrame, textvariable=self.currentPenColorVar)
 		self.currentPenColorLabel.pack(side=LEFT,padx=50)
 
-		self.currentPixelColorLabel=Label(self.PenOptionFrame, text="Couleur actuelle!")
+		self.currentPixelColorVar=StringVar()
+		self.currentPixelColorVar.set(CURRENT_PIXEL_TEXT.format(0,0,0) )
+		self.currentPixelColorLabel=Label(self.PenOptionFrame, textvariable=self.currentPixelColorVar)
 		self.currentPixelColorLabel.pack(side=LEFT,padx=50)
 
 		#Upload option
 		self.uploadButton = Button(master, text="Upload", command=self.upload)
 		self.uploadButton.pack()
 
+	#Upload, load, save functions
 	def load(self):
-		image_file=filedialog.askopenfilename(initialdir = ".",title = "Select Image",
+		image_file=askopenfilename(initialdir = ".",title = "Select Image",
 			filetypes = (("Images files","*.jpg *.png"),("all files","*.*")))
 		self.import_image(image_file)
 		self.drawMatrix()
 
 	def save(self):
-		print("Save image")
+		image_file=asksaveasfilename(initialdir = ".",title = "Select Image",
+			filetypes = (("Images files","*.jpg *.png"),("all files","*.*")))
+
+		np_matrix=np.array(self.matrix,dtype=np.uint8).transpose((1,0,2))#inverse x and y
+
+		new_image=Image.fromarray(np_matrix,mode='RGB')
+		new_image.save(image_file, 'PNG',dpi=[32,16])
 
 	def upload(self):
 		print("Upload!")
 
 	def import_image(self,fichier):
+		if type(fichier)==tuple or fichier=="":
+			return
 		try:
 			im = Image.open(fichier)
 		except Exception as e:
 			print("Erreur : ",e)
-			return 0
+			return
 		image_data=im.resize( (32,16) ,Image.BICUBIC).getdata()
 		self.matrix=[[(0,0,0)for y in range(16)]for x in range(32)]
 		for i in range(len(image_data)):
 			self.matrix[i%32][i/32]=image_data[i][:3]
 
-	def getColor(self):
+	#Pen functions
+	def setColor(self):
 		self.currentColor= askcolor()[0]
-	def penCallback(self,event):
+		self.currentPenColorVar.set(CURRENT_COLOR_TEXT.format(
+			self.currentColor[0],
+			self.currentColor[1],
+			self.currentColor[2]))
+
+	def getXYmatrixFromMouse(self,event):
 		if event.x<0 or event.y<0:
-			return
+			return -1,-1
 		x=event.x/LED_PIXELS_SIZE
 		y=event.y/LED_PIXELS_SIZE
 		if x>=32 or y>=16:
-			return
+			return -1,-1
+		return x,y
+
+	def penCallback(self,event):
+		x,y=self.getXYmatrixFromMouse(event)
+		if x<0 or y<0:return
 		self.matrix[x][y]=self.currentColor
 		self.drawMatrix()
 
+	def moveMouseOverPixel(self,event):
+		x,y=self.getXYmatrixFromMouse(event)
+		if x<0 or y<0:return
+		self.currentPixelColorVar.set(CURRENT_PIXEL_TEXT.format(
+			self.matrix[x][y][0],
+			self.matrix[x][y][1],
+			self.matrix[x][y][2]))
 
+	#draw functions
 	def getFillColor(self,x,y):
 		return "#"+"".join(["{:02x}".format(i) for i in self.matrix [x][y]])
+
 	def drawMatrix(self):
 		self.canvasMatrix.delete("all")
 		for x in range(32):
